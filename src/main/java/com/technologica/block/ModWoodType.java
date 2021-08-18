17ca1b9a88d44db232979d24eb3759cd85353178
burnsqc <jonathan.r.mobley@gmail.com>
2021-08-17 21:49:25 -0600

    ALL wood derivatives except boats registered

--- src/main/java/com/technologica/block/ModWoodType.java
+++ src/main/java/com/technologica/block/ModWoodType.java
@@ -7,15 +7,36 @@ import net.minecraft.block.WoodType;
 
 public class ModWoodType extends WoodType {
 	private static final Set<WoodType> VALUES = new ObjectArraySet<>();
+	public static final WoodType APRICOT = register(new ModWoodType("apricot"));
+	public static final WoodType ASPEN = register(new ModWoodType("aspen"));
+	public static final WoodType AVOCADO = register(new ModWoodType("avocado"));
    	public static final WoodType BANANA = register(new ModWoodType("banana"));
    	public static final WoodType CHERRY = register(new ModWoodType("cherry"));
+   	public static final WoodType CHESTNUT = register(new ModWoodType("chestnut"));
    	public static final WoodType COCONUT = register(new ModWoodType("coconut"));
+   	public static final WoodType EBONY = register(new ModWoodType("ebony"));
    	public static final WoodType KIWI = register(new ModWoodType("kiwi"));
    	public static final WoodType LEMON = register(new ModWoodType("lemon"));
    	public static final WoodType LIME = register(new ModWoodType("lime"));
+   	public static final WoodType MAHOGANY = register(new ModWoodType("mahogany"));
+   	public static final WoodType MAPLE = register(new ModWoodType("maple"));
    	public static final WoodType ORANGE = register(new ModWoodType("orange"));
    	public static final WoodType PEACH = register(new ModWoodType("peach"));
    	public static final WoodType PEAR = register(new ModWoodType("pear"));
+   	public static final WoodType PLUM = register(new ModWoodType("plum"));
+   	public static final WoodType REDWOOD = register(new ModWoodType("redwood"));
+   	public static final WoodType ROSEWOOD = register(new ModWoodType("rosewood"));
+   	public static final WoodType RUBBER = register(new ModWoodType("rubber"));
+   	public static final WoodType TEAK = register(new ModWoodType("teak"));
+   	public static final WoodType WALNUT = register(new ModWoodType("walnut"));
+   	public static final WoodType ZEBRAWOOD = register(new ModWoodType("zebrawood"));
+   	public static final WoodType ALCHEMICAL = register(new ModWoodType("alchemical"));
+   	public static final WoodType BENEVOLENT = register(new ModWoodType("benevolent"));
+   	public static final WoodType CONDUCTIVE = register(new ModWoodType("conductive"));
+   	public static final WoodType FROSTBITTEN = register(new ModWoodType("frostbitten"));
+   	public static final WoodType FRUITFUL = register(new ModWoodType("fruitful"));
+   	public static final WoodType INFERNAL = register(new ModWoodType("infernal"));
+   	public static final WoodType MALEVOLENT = register(new ModWoodType("malevolent"));
    	private final String name;
 
    	protected ModWoodType(String nameIn) {
--- src/main/java/com/technologica/tileentity/ModTileEntities.java
+++ src/main/java/com/technologica/tileentity/ModTileEntities.java
@@ -11,17 +11,93 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModTileEntities {
-	private static final DeferredRegister<TileEntityType<?>> TILEENTITIES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, MODID);
+	private static final DeferredRegister<TileEntityType<?>> TILEENTITIES = DeferredRegister
+			.create(ForgeRegistries.TILE_ENTITIES, MODID);
 
-	public static final RegistryObject<TileEntityType<ModSignTileEntity>> MOD_SIGN = TILEENTITIES.register("mod_sign", () -> TileEntityType.Builder.create(ModSignTileEntity::new, ModBlocks.BANANA_SIGN.get(), ModBlocks.CHERRY_SIGN.get(), ModBlocks.COCONUT_SIGN.get(), ModBlocks.KIWI_SIGN.get(), ModBlocks.LEMON_SIGN.get(), ModBlocks.LIME_SIGN.get(), ModBlocks.ORANGE_SIGN.get(), ModBlocks.PEACH_SIGN.get(), ModBlocks.PEAR_SIGN.get(), ModBlocks.BANANA_WALL_SIGN.get(), ModBlocks.CHERRY_WALL_SIGN.get(), ModBlocks.COCONUT_WALL_SIGN.get(), ModBlocks.KIWI_WALL_SIGN.get(), ModBlocks.LEMON_WALL_SIGN.get(), ModBlocks.LIME_WALL_SIGN.get(), ModBlocks.ORANGE_WALL_SIGN.get(), ModBlocks.PEACH_WALL_SIGN.get(), ModBlocks.PEAR_WALL_SIGN.get()).build(null));
-	public static final RegistryObject<TileEntityType<FruitTileEntity>> FRUIT_CROP = TILEENTITIES.register("fruit_crop", () -> TileEntityType.Builder.create(FruitTileEntity::new, ModBlocks.BANANA_LEAVES.get(), ModBlocks.CHERRY_LEAVES.get(), ModBlocks.COCONUT_LEAVES.get(), ModBlocks.KIWI_LEAVES.get(), ModBlocks.LEMON_LEAVES.get(), ModBlocks.LIME_LEAVES.get(), ModBlocks.ORANGE_LEAVES.get(), ModBlocks.PEACH_LEAVES.get(), ModBlocks.PEAR_LEAVES.get(), ModBlocks.FRUITFUL_LEAVES.get(), ModBlocks.ALCHEMICAL_LEAVES.get()).build(null));
-	public static final RegistryObject<TileEntityType<PotionTileEntity>> POTION_CROP = TILEENTITIES.register("potion_crop", () -> TileEntityType.Builder.create(PotionTileEntity::new, ModBlocks.BENEVOLENT_LEAVES.get(), ModBlocks.MALEVOLENT_LEAVES.get()).build(null));
-	public static final RegistryObject<TileEntityType<DisplayCaseTileEntity>> DISPLAY_TILE = TILEENTITIES.register("display_tile", () -> TileEntityType.Builder.create(DisplayCaseTileEntity::new, ModBlocks.DISPLAY_CASE.get()).build(null));
-	public static final RegistryObject<TileEntityType<LineShaftTileEntity>> LINE_SHAFT_TILE = TILEENTITIES.register("line_shaft_tile", () -> TileEntityType.Builder.create(LineShaftTileEntity::new, ModBlocks.LINE_SHAFT.get()).build(null));
-	public static final RegistryObject<TileEntityType<LineShaftHangerTileEntity>> LINE_SHAFT_HANGER_TILE = TILEENTITIES.register("line_shaft_hanger_tile", () -> TileEntityType.Builder.create(LineShaftHangerTileEntity::new, ModBlocks.LINE_SHAFT_HANGER.get()).build(null));
-	
-	public static void register() 
-	{
-        TILEENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
+	public static final RegistryObject<TileEntityType<ModSignTileEntity>> MOD_SIGN = TILEENTITIES.register("mod_sign",
+			() -> TileEntityType.Builder.create(ModSignTileEntity::new,
+					ModBlocks.APRICOT_SIGN.get(),
+					ModBlocks.ASPEN_SIGN.get(),
+					ModBlocks.AVOCADO_SIGN.get(),
+					ModBlocks.BANANA_SIGN.get(),
+					ModBlocks.CHERRY_SIGN.get(),
+					ModBlocks.CHESTNUT_SIGN.get(),
+					ModBlocks.COCONUT_SIGN.get(),
+					ModBlocks.EBONY_SIGN.get(),
+					ModBlocks.KIWI_SIGN.get(),
+					ModBlocks.LEMON_SIGN.get(), 
+					ModBlocks.LIME_SIGN.get(),
+					ModBlocks.MAHOGANY_SIGN.get(),
+					ModBlocks.MAPLE_SIGN.get(),
+					ModBlocks.ORANGE_SIGN.get(),
+					ModBlocks.PEACH_SIGN.get(), 
+					ModBlocks.PEAR_SIGN.get(),
+					ModBlocks.PLUM_SIGN.get(),
+					ModBlocks.REDWOOD_SIGN.get(),
+					ModBlocks.ROSEWOOD_SIGN.get(),
+					ModBlocks.RUBBER_SIGN.get(),
+					ModBlocks.TEAK_SIGN.get(),
+					ModBlocks.WALNUT_SIGN.get(),
+					ModBlocks.ZEBRAWOOD_SIGN.get(),
+					ModBlocks.ALCHEMICAL_SIGN.get(),
+					ModBlocks.BENEVOLENT_SIGN.get(),
+					ModBlocks.CONDUCTIVE_SIGN.get(),
+					ModBlocks.FROSTBITTEN_SIGN.get(),
+					ModBlocks.FRUITFUL_SIGN.get(),
+					ModBlocks.INFERNAL_SIGN.get(),
+					ModBlocks.MALEVOLENT_SIGN.get(),
+					ModBlocks.APRICOT_WALL_SIGN.get(),
+					ModBlocks.ASPEN_WALL_SIGN.get(),
+					ModBlocks.AVOCADO_WALL_SIGN.get(),
+					ModBlocks.BANANA_WALL_SIGN.get(),
+					ModBlocks.CHERRY_WALL_SIGN.get(),
+					ModBlocks.CHESTNUT_WALL_SIGN.get(),
+					ModBlocks.COCONUT_WALL_SIGN.get(),
+					ModBlocks.EBONY_WALL_SIGN.get(),
+					ModBlocks.KIWI_WALL_SIGN.get(),
+					ModBlocks.LEMON_WALL_SIGN.get(), 
+					ModBlocks.LIME_WALL_SIGN.get(),
+					ModBlocks.MAHOGANY_WALL_SIGN.get(),
+					ModBlocks.MAPLE_WALL_SIGN.get(),
+					ModBlocks.ORANGE_WALL_SIGN.get(),
+					ModBlocks.PEACH_WALL_SIGN.get(), 
+					ModBlocks.PEAR_WALL_SIGN.get(),
+					ModBlocks.PLUM_WALL_SIGN.get(),
+					ModBlocks.REDWOOD_WALL_SIGN.get(),
+					ModBlocks.ROSEWOOD_WALL_SIGN.get(),
+					ModBlocks.RUBBER_WALL_SIGN.get(),
+					ModBlocks.TEAK_WALL_SIGN.get(),
+					ModBlocks.WALNUT_WALL_SIGN.get(),
+					ModBlocks.ZEBRAWOOD_WALL_SIGN.get(),
+					ModBlocks.ALCHEMICAL_WALL_SIGN.get(),
+					ModBlocks.BENEVOLENT_WALL_SIGN.get(),
+					ModBlocks.CONDUCTIVE_WALL_SIGN.get(),
+					ModBlocks.FROSTBITTEN_WALL_SIGN.get(),
+					ModBlocks.FRUITFUL_WALL_SIGN.get(),
+					ModBlocks.INFERNAL_WALL_SIGN.get(),
+					ModBlocks.MALEVOLENT_WALL_SIGN.get()).build(null));
+	public static final RegistryObject<TileEntityType<FruitTileEntity>> FRUIT_CROP = TILEENTITIES.register("fruit_crop",
+			() -> TileEntityType.Builder.create(FruitTileEntity::new, ModBlocks.BANANA_LEAVES.get(),
+					ModBlocks.CHERRY_LEAVES.get(), ModBlocks.COCONUT_LEAVES.get(), ModBlocks.KIWI_LEAVES.get(),
+					ModBlocks.LEMON_LEAVES.get(), ModBlocks.LIME_LEAVES.get(), ModBlocks.ORANGE_LEAVES.get(),
+					ModBlocks.PEACH_LEAVES.get(), ModBlocks.PEAR_LEAVES.get(), ModBlocks.FRUITFUL_LEAVES.get(),
+					ModBlocks.ALCHEMICAL_LEAVES.get()).build(null));
+	public static final RegistryObject<TileEntityType<PotionTileEntity>> POTION_CROP = TILEENTITIES.register(
+			"potion_crop",
+			() -> TileEntityType.Builder
+					.create(PotionTileEntity::new, ModBlocks.BENEVOLENT_LEAVES.get(), ModBlocks.MALEVOLENT_LEAVES.get())
+					.build(null));
+	public static final RegistryObject<TileEntityType<DisplayCaseTileEntity>> DISPLAY_TILE = TILEENTITIES.register(
+			"display_tile",
+			() -> TileEntityType.Builder.create(DisplayCaseTileEntity::new, ModBlocks.DISPLAY_CASE.get()).build(null));
+	public static final RegistryObject<TileEntityType<LineShaftTileEntity>> LINE_SHAFT_TILE = TILEENTITIES.register(
+			"line_shaft_tile",
+			() -> TileEntityType.Builder.create(LineShaftTileEntity::new, ModBlocks.LINE_SHAFT.get()).build(null));
+	public static final RegistryObject<TileEntityType<LineShaftHangerTileEntity>> LINE_SHAFT_HANGER_TILE = TILEENTITIES
+			.register("line_shaft_hanger_tile", () -> TileEntityType.Builder
+					.create(LineShaftHangerTileEntity::new, ModBlocks.LINE_SHAFT_HANGER.get()).build(null));
+
+	public static void register() {
+		TILEENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
 	}
 }
