f10ba848ad6f905b00a412ba7a43c65027e458c6
burnsqc <jonathan.r.mobley@gmail.com>
2021-10-22 10:37:47 -0600

    Update LowGrav.java

--- src/main/java/com/technologica/util/LowGrav.java
+++ src/main/java/com/technologica/util/LowGrav.java
@@ -7,27 +7,24 @@ import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class LowGrav {
-	private ClientPlayerEntity client;
 	
 	@SubscribeEvent
 	public void onLivingJumpEvent(LivingJumpEvent event) {
-		if (event.getEntity().world.getDimensionKey().toString().contains("technologica:moon")) {
+		if (event.getEntity().world.getDimensionKey().toString().contains("technologica:moon") && event.getEntity().world.isRemote) {
 			if (event.getEntity() instanceof ClientPlayerEntity) {
-				client = (ClientPlayerEntity) event.getEntity();
-				
 				Minecraft mc = Minecraft.getInstance();
 				
 			    if (mc.gameSettings.keyBindJump.isKeyDown()) {
-				    client.addVelocity(0, 1, 0);
-				    client.velocityChanged = true;
+			    	event.getEntity().addVelocity(0, 1, 0);
+			    	event.getEntity().velocityChanged = true;
 			    }
-		    }
+			}
 		}
 	}
 				
 	@SubscribeEvent
 	public void onLivingFallEvent(LivingFallEvent event) {
-		if (event.getEntity().world.getDimensionKey().toString().contains("technologica:moon")) {
+ 		if (event.getEntity().world.getDimensionKey().toString().contains("technologica:moon")) {
 			event.setDamageMultiplier(0F);
 		}
 	}
