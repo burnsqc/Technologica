68011aac697969dac5396cd63e713747b250a99d
theseeker1300 <theseeker0175@gmail.com>
2021-12-19 10:11:56 -0700

    Update PenguinModel.java

--- src/main/java/com/technologica/client/renderer/entity/model/PenguinModel.java
+++ src/main/java/com/technologica/client/renderer/entity/model/PenguinModel.java
@@ -19,16 +19,16 @@ public class PenguinModel<T extends AnimalEntity> extends AgeableModel<T> {
 
 	public PenguinModel() {
 		this.head = new ModelRenderer(this, 0, 0);
-		this.head.addBox(-2.0F, -6.0F, -2.0F, 4.0F, 6.0F, 3.0F, 0.0F);
+		this.head.addBox(-2.0F, -11.0F, 2.0F, 4.0F, 4.0F, 4.0F, 0.0F);
 		this.head.setRotationPoint(0.0F, 15.0F, -4.0F);
 		this.bill = new ModelRenderer(this, 14, 0);
-		this.bill.addBox(-2.0F, -4.0F, -4.0F, 4.0F, 2.0F, 2.0F, 0.0F);
+		this.bill.addBox(-2.0F, -9.0F, 0.0F, 4.0F, 1.0F, 3.0F, 0.0F);
 		this.bill.setRotationPoint(0.0F, 15.0F, -4.0F);
 		this.chin = new ModelRenderer(this, 14, 4);
-		this.chin.addBox(-1.0F, -2.0F, -3.0F, 2.0F, 2.0F, 2.0F, 0.0F);
+		this.chin.addBox(-1.0F, -8.0F, 1.0F, 1.0F, 1.0F, 4.0F, 0.0F);
 		this.chin.setRotationPoint(0.0F, 15.0F, -4.0F);
 		this.body = new ModelRenderer(this, 0, 9);
-		this.body.addBox(-3.0F, -4.0F, -3.0F, 6.0F, 8.0F, 6.0F, 0.0F);
+		this.body.addBox(-3.0F, -8.0F, -3.0F, 6.0F, 14.0F, 6.0F, 0.0F);
 		this.body.setRotationPoint(0.0F, 16.0F, 0.0F);
 		this.rightLeg = new ModelRenderer(this, 26, 0);
 		this.rightLeg.addBox(-1.0F, 0.0F, -3.0F, 3.0F, 5.0F, 3.0F);
@@ -37,10 +37,10 @@ public class PenguinModel<T extends AnimalEntity> extends AgeableModel<T> {
 		this.leftLeg.addBox(-1.0F, 0.0F, -3.0F, 3.0F, 5.0F, 3.0F);
 		this.leftLeg.setRotationPoint(1.0F, 19.0F, 1.0F);
 		this.rightWing = new ModelRenderer(this, 24, 13);
-		this.rightWing.addBox(0.0F, 0.0F, -3.0F, 1.0F, 4.0F, 6.0F);
+		this.rightWing.addBox(0.0F, -3.0F, -1.0F, 1.0F, 8.0F, 3.0F);
 		this.rightWing.setRotationPoint(-4.0F, 13.0F, 0.0F);
 		this.leftWing = new ModelRenderer(this, 24, 13);
-		this.leftWing.addBox(-1.0F, 0.0F, -3.0F, 1.0F, 4.0F, 6.0F);
+		this.leftWing.addBox(-1.0F, -3.0F, -1.0F, 1.0F, 8.0F, 3.0F);
 		this.leftWing.setRotationPoint(4.0F, 13.0F, 0.0F);
 	}
 
