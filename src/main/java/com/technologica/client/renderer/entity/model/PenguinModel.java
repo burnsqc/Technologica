ff2bf5996da0724de9d257aae26a0e0b4a6e2d1f
theseeker1300 <theseeker0175@gmail.com>
2021-12-21 15:29:56 -0700

    penguin

--- src/main/java/com/technologica/client/renderer/entity/model/PenguinModel.java
+++ src/main/java/com/technologica/client/renderer/entity/model/PenguinModel.java
@@ -15,38 +15,37 @@ public class PenguinModel<T extends AnimalEntity> extends AgeableModel<T> {
 	private final ModelRenderer rightWing;
 	private final ModelRenderer leftWing;
 	private final ModelRenderer bill;
-	private final ModelRenderer chin;
 
 	public PenguinModel() {
+		this.textureWidth = 32;
+		this.textureHeight = 32;
+		
 		this.head = new ModelRenderer(this, 0, 0);
-		this.head.addBox(-2.0F, -11.0F, 2.0F, 4.0F, 4.0F, 4.0F, 0.0F);
+		this.head.addBox(-2.0F, -12.0F, 2.0F, 4.0F, 4.0F, 4.0F, 0.0F);
 		this.head.setRotationPoint(0.0F, 15.0F, -4.0F);
-		this.bill = new ModelRenderer(this, 14, 0);
-		this.bill.addBox(-2.0F, -9.0F, 0.0F, 4.0F, 1.0F, 3.0F, 0.0F);
+		this.bill = new ModelRenderer(this, 0, 8);
+		this.bill.addBox(-1.0F, -10.0F, 0.0F, 2.0F, 1.0F, 2.0F, 0.0F);
 		this.bill.setRotationPoint(0.0F, 15.0F, -4.0F);
-		this.chin = new ModelRenderer(this, 14, 4);
-		this.chin.addBox(-1.0F, -8.0F, 1.0F, 1.0F, 1.0F, 4.0F, 0.0F);
-		this.chin.setRotationPoint(0.0F, 15.0F, -4.0F);
-		this.body = new ModelRenderer(this, 0, 9);
-		this.body.addBox(-3.0F, -8.0F, -3.0F, 6.0F, 14.0F, 6.0F, 0.0F);
+		this.body = new ModelRenderer(this, 8, 9);
+		this.body.addBox(-3.0F, -9.0F, -3.0F, 6.0F, 14.0F, 6.0F, 0.0F);
 		this.body.setRotationPoint(0.0F, 16.0F, 0.0F);
-		this.rightLeg = new ModelRenderer(this, 26, 0);
-		this.rightLeg.addBox(-1.0F, 0.0F, -3.0F, 3.0F, 5.0F, 3.0F);
+		this.rightLeg = new ModelRenderer(this, 18, 0);
+		this.rightLeg.addBox(-1.0F, 2.0F, -3.0F, 3.0F, 3.0F, 3.0F);
 		this.rightLeg.setRotationPoint(-2.0F, 19.0F, 1.0F);
-		this.leftLeg = new ModelRenderer(this, 26, 0);
-		this.leftLeg.addBox(-1.0F, 0.0F, -3.0F, 3.0F, 5.0F, 3.0F);
+		this.leftLeg = new ModelRenderer(this, 18, 0);
+		this.leftLeg.addBox(-1.0F, 2.0F, -3.0F, 3.0F, 3.0F, 3.0F);
 		this.leftLeg.setRotationPoint(1.0F, 19.0F, 1.0F);
-		this.rightWing = new ModelRenderer(this, 24, 13);
-		this.rightWing.addBox(0.0F, -3.0F, -1.0F, 1.0F, 8.0F, 3.0F);
+		this.rightWing = new ModelRenderer(this, 0, 12);
+		this.rightWing.addBox(0.0F, -4.0F, -1.0F, 1.0F, 8.0F, 3.0F);
 		this.rightWing.setRotationPoint(-4.0F, 13.0F, 0.0F);
-		this.leftWing = new ModelRenderer(this, 24, 13);
-		this.leftWing.addBox(-1.0F, -3.0F, -1.0F, 1.0F, 8.0F, 3.0F);
+		this.leftWing = new ModelRenderer(this, 0, 12);
+		this.leftWing.addBox(-1.0F, -4.0F, -1.0F, 1.0F, 8.0F, 3.0F);
 		this.leftWing.setRotationPoint(4.0F, 13.0F, 0.0F);
 	}
 
 	@Override
 	protected Iterable<ModelRenderer> getHeadParts() {
-		return ImmutableList.of(this.head, this.bill, this.chin);
+		return ImmutableList.of(this.head, this.bill);
 	}
 
 	@Override
