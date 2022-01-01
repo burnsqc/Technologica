8d8ade3450783964ae7d85514202590a31899774
theseeker1300 <theseeker0175@gmail.com>
2022-01-01 13:47:21 -0700

    frog

--- src/main/java/com/technologica/client/renderer/entity/model/FrogModel.java
+++ src/main/java/com/technologica/client/renderer/entity/model/FrogModel.java
@@ -9,34 +9,36 @@ public class FrogModel<T extends FrogEntity> extends QuadrupedModel<T> {
 
 	public FrogModel() {
 		super(2, 0.0F, true, 10.0F, 4.0F, 2.0F, 2.0F, 24);
+		this.textureWidth = 32;
+		this.textureHeight = 16;
 		
 		this.body = new ModelRenderer(this, 0, 0);
 		this.body.addBox(-2.0F, -1.0F, -2.0F, 4.0F, 3.0F, 4.0F);
 		this.body.setRotationPoint(0.0F, 21.0F, 0.0F);
 		
-		this.headModel = new ModelRenderer(this, 0, 14);
+		this.headModel = new ModelRenderer(this, 0, 7);
 		this.headModel.addBox(-1.5F, -1.0F, -3.0F, 3.0F, 2.0F, 3.0F);
 		this.headModel.setRotationPoint(0.0F, 20.0F, -1.0F);
 		
-		this.headModel.setTextureOffset(22, 0).addBox(-1.5F, -2.0F, -2.0F, 1.0F, 1.0F, 2.0F);
-		this.headModel.setTextureOffset(22, 0).addBox(0.5F, -2.0F, -2.0F, 1.0F, 1.0F, 2.0F);
+		this.headModel.setTextureOffset(0, 12).addBox(-1.5F, -2.0F, -2.0F, 1.0F, 1.0F, 2.0F);
+		this.headModel.setTextureOffset(0, 12).addBox(0.5F, -2.0F, -2.0F, 1.0F, 1.0F, 2.0F);
 		
-		this.legBackRight = new ModelRenderer(this, 0, 16);
+		this.legBackRight = new ModelRenderer(this, 12, 0);
 	    this.legBackRight.addBox(0.0F, 0.0F, 0.0F, 1.0F, 3.0F, 1.0F);
 	    this.legBackRight.setRotationPoint(-2.0F, 21.0F, 1.0F);
 	    this.legBackRight.addBox(-0.5F, 3.0F, -1.0F, 2.0F, 0.0F, 2.0F);
 	    
-	    this.legBackLeft = new ModelRenderer(this, 0, 16);
+	    this.legBackLeft = new ModelRenderer(this, 12, 0);
 	    this.legBackLeft.addBox(0.0F, 0.0F, 0.0F, 1.0F, 3.0F, 1.0F);
 	    this.legBackLeft.setRotationPoint(1.0F, 21.0F, 1.0F);
 	    this.legBackLeft.addBox(-0.5F, 3.0F, -1.0F, 2.0F, 0.0F, 2.0F);
 	    
-	    this.legFrontRight = new ModelRenderer(this, 0, 16);
+	    this.legFrontRight = new ModelRenderer(this, 12, 0);
 	    this.legFrontRight.addBox(0.0F, 0.0F, 0.0F, 1.0F, 3.0F, 1.0F);
 	    this.legFrontRight.setRotationPoint(-2.0F, 21.0F, -2.0F);
 	    this.legFrontRight.addBox(-0.5F, 3.0F, -1.0F, 2.0F, 0.0F, 2.0F);
 	    
-	    this.legFrontLeft = new ModelRenderer(this, 0, 16);
+	    this.legFrontLeft = new ModelRenderer(this, 12, 0);
 	    this.legFrontLeft.addBox(0.0F, 0.0F, 0.0F, 1.0F, 3.0F, 1.0F);
 	    this.legFrontLeft.setRotationPoint(1.0F, 21.0F, -2.0F);
 	    this.legFrontLeft.addBox(-0.5F, 3.0F, -1.0F, 2.0F, 0.0F, 2.0F);
