3271bf74e201eb93d9b0e7b6caa60aaa6145b446
theseeker1300 <theseeker0175@gmail.com>
2022-01-01 13:10:46 -0700

    piranha

--- src/main/java/com/technologica/client/renderer/entity/model/PiranhaModel.java
+++ src/main/java/com/technologica/client/renderer/entity/model/PiranhaModel.java
@@ -16,30 +16,30 @@ public class PiranhaModel<T extends Entity> extends AbstractTropicalFishModel<T>
 	private final ModelRenderer finBottom;
 
 	public PiranhaModel() {
-		this.textureWidth = 32;
-		this.textureHeight = 32;
+		this.textureWidth = 16;
+		this.textureHeight = 16;
 		
-		this.body = new ModelRenderer(this, 0, 20);
+		this.body = new ModelRenderer(this, 0, 1);
 		this.body.addBox(-1.0F, -3.0F, -3.0F, 2.0F, 6.0F, 6.0F);
 		this.body.setRotationPoint(0.0F, 21.0F, 6.0F);
 		
-		this.tail = new ModelRenderer(this, 21, 16);
+		this.tail = new ModelRenderer(this, 0, 0);
 		this.tail.addBox(0.0F, -2.5F, 0.0F, 0.0F, 5.0F, 2.0F);
 		this.tail.setRotationPoint(0.0F, 21.0F, 9.0F);
 		
-		this.finLeft = new ModelRenderer(this, 2, 12);
+		this.finLeft = new ModelRenderer(this, 0, 1);
 		this.finLeft.addBox(0.0F, 0.0F, 0.0F, 2.0F, 1.0F, 0.0F);
 		this.finLeft.setRotationPoint(1.0F, 22.0F, 4.0F);
 		
-		this.finRight = new ModelRenderer(this, 2, 16);
+		this.finRight = new ModelRenderer(this, 0, 1);
 		this.finRight.addBox(-2.0F, 0.0F, 0.0F, 2.0F, 1.0F, 0.0F);
 		this.finRight.setRotationPoint(-1.0F, 22.0F, 4.0F);
 		
-		this.finTop = new ModelRenderer(this, 20, 11);
+		this.finTop = new ModelRenderer(this, 10, 1);
 		this.finTop.addBox(0.0F, -4.0F, -2.0F, 0.0F, 1.0F, 2.0F);
 		this.finTop.setRotationPoint(0.0F, 21.0F, 6.0F);
 		
-		this.finBottom = new ModelRenderer(this, 20, 21);
+		this.finBottom = new ModelRenderer(this, 10, 1);
 		this.finBottom.addBox(0.0F, 3.0F, -1.0F, 0.0F, 1.0F, 2.0F);
 		this.finBottom.setRotationPoint(0.0F, 21.0F, 6.0F);
 	}
