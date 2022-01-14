f3b9579a6e66a8f8ef2b74c59f599e8ea6e6b28e
burnsqc <jonathan.r.mobley@gmail.com>
2022-01-14 06:23:41 -0700

    Walrus

--- src/main/java/com/technologica/client/renderer/entity/model/WalrusModel.java
+++ src/main/java/com/technologica/client/renderer/entity/model/WalrusModel.java
@@ -23,29 +23,29 @@ public class WalrusModel<T extends Entity> extends SegmentedModel<T> {
 	private final ModelRenderer flipperBackRight;
 
 	public WalrusModel() {
-		this.textureWidth = 64;
+		this.textureWidth = 128;
 		this.textureHeight = 64;
 
-		this.body = new ModelRenderer(this, 22, 0);
+		this.body = new ModelRenderer(this, 0, 0);
 		this.body.addBox(-10.0F, -10.0F, -8.0F, 20.0F, 20.0F, 16.0F);
 		this.body.setRotationPoint(0.0F, 14.0F, -8.0F);
 
-		this.tail = new ModelRenderer(this, 0, 35);
+		this.tail = new ModelRenderer(this, 56, 20);
 		this.tail.addBox(-8.0F, -8.0F, 0.0F, 16.0F, 16.0F, 16.0F);
 		this.tail.setRotationPoint(0.0F, 2.0F, 8.0F);
 		this.body.addChild(this.tail);
 		
-		this.tail2 = new ModelRenderer(this, 0, 35);
+		this.tail2 = new ModelRenderer(this, 56, 0);
 		this.tail2.addBox(-4.0F, -4.0F, 0.0F, 8.0F, 8.0F, 8.0F);
 		this.tail2.setRotationPoint(0.0F, 4.0F, 16.0F);
 		this.tail.addChild(this.tail2);
 		
-		this.head = new ModelRenderer(this, 0, 0);
+		this.head = new ModelRenderer(this, 88, 0);
 		this.head.addBox(-5.0F, -3.0F, -3.0F, 10.0F, 10.0F, 10.0F);
 		this.head.setRotationPoint(0.0F, -10.0F, -8.0F);
 		this.body.addChild(this.head);
 		
-		this.nose = new ModelRenderer(this, 0, 0);
+		this.nose = new ModelRenderer(this, 104, 20);
 		this.nose.addBox(-4.0F, 2.0F, -5.0F, 8.0F, 4.0F, 2.0F);
 		this.head.addChild(this.nose);
 		
@@ -57,23 +57,23 @@ public class WalrusModel<T extends Entity> extends SegmentedModel<T> {
 		this.tuskRight.addBox(2.0F, 6.0F, -4.0F, 1.0F, 6.0F, 1.0F);
 		this.head.addChild(this.tuskRight);
 
-		this.flipperFrontLeft = new ModelRenderer(this, 48, 20);
+		this.flipperFrontLeft = new ModelRenderer(this, 104, 26);
 		this.flipperFrontLeft.mirror = true;
 		this.flipperFrontLeft.addBox(0.0F, -1.0F, -5.0F, 7.0F, 2.0F, 5.0F);
 		this.flipperFrontLeft.setRotationPoint(10.0F, 9.0F, -2.0F);
 		this.body.addChild(this.flipperFrontLeft);
 
-		this.flipperFrontRight = new ModelRenderer(this, 48, 20);
+		this.flipperFrontRight = new ModelRenderer(this, 104, 26);
 		this.flipperFrontRight.addBox(-7.0F, -1.0F, -5.0F, 7.0F, 2.0F, 5.0F);
 		this.flipperFrontRight.setRotationPoint(-10.0F, 9.0F, -2.0F);
 		this.body.addChild(this.flipperFrontRight);
 
-		this.flipperBackLeft = new ModelRenderer(this, 30, 20);
+		this.flipperBackLeft = new ModelRenderer(this, 104, 26);
 		this.flipperBackLeft.addBox(0.0F, -1.0F, 0.0F, 7.0F, 2.0F, 5.0F);
 		this.flipperBackLeft.setRotationPoint(4.0F, 3.0F, 3.0F);
 		this.tail2.addChild(this.flipperBackLeft);
 
-		this.flipperBackRight = new ModelRenderer(this, 13, 17);
+		this.flipperBackRight = new ModelRenderer(this, 104, 26);
 		this.flipperBackRight.addBox(-7.0F, -1.0F, 0.0F, 7.0F, 2.0F, 5.0F);
 		this.flipperBackRight.setRotationPoint(-4.0F, 3.0F, 3.0F);
 		this.tail2.addChild(this.flipperBackRight);
