8d6a5b4dd2f6a78b63cf9646b42dc57d0629ce9c
burnsqc <jonathan.r.mobley@gmail.com>
2022-01-22 08:57:57 -0700

    Jaguar model and texture

--- src/main/java/com/technologica/client/renderer/entity/model/JaguarModel.java
+++ src/main/java/com/technologica/client/renderer/entity/model/JaguarModel.java
@@ -6,10 +6,7 @@ import com.technologica.entity.passive.JaguarEntity;
 import net.minecraft.client.renderer.entity.model.AgeableModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.util.math.MathHelper;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
-@OnlyIn(Dist.CLIENT)
 public class JaguarModel<T extends JaguarEntity> extends AgeableModel<T> {
 	protected final ModelRenderer legBackLeft;
 	protected final ModelRenderer legBackRight;
@@ -26,50 +23,54 @@ public class JaguarModel<T extends JaguarEntity> extends AgeableModel<T> {
 
 	public JaguarModel() {
 		super(true, 10.0F, 4.0F);
-		this.head = new ModelRenderer(this, 0, 0);
+		this.textureWidth = 64;
+		this.textureHeight = 32;
+		
+		this.head = new ModelRenderer(this, 32, 0);
 		this.head.setRotationPoint(0.0F, 9.0F, -6.0F);
 		this.head.addBox(-3.0F, -3.0F, -6.0F, 6.0F, 6.0F, 6.0F);
 		
-		this.nose = new ModelRenderer(this, 0, 0);
-		this.nose.addBox(-2.0F, 0.0F, -8.0F, 4.0F, 3.0F, 2.0F);
+		this.nose = new ModelRenderer(this, 24, 0);
+		this.nose.addBox(-1.5F, 0.0F, -8.0F, 3.0F, 3.0F, 2.0F);
 		this.head.addChild(this.nose);
 		
-		this.earRight = new ModelRenderer(this, 0, 16);
-		this.earRight.addBox(0.0F, -1.0F, 0.0F, 1.0F, 2.0F, 1.0F);
+		this.earRight = new ModelRenderer(this, 50, 0);
+		this.earRight.addBox(-1.0F, -1.0F, 0.0F, 2.0F, 2.0F, 1.0F);
 		this.earRight.setRotationPoint(-3.0F, -3.0F, -3.0F);
 		this.head.addChild(this.earRight);
 
-		this.earLeft = new ModelRenderer(this, 0, 16);
-		this.earLeft.addBox(-1.0F, -1.0F, 0.0F, 1.0F, 2.0F, 1.0F);
+		this.earLeft = new ModelRenderer(this, 50, 0);
+		this.earLeft.addBox(-1.0F, -1.0F, 0.0F, 2.0F, 2.0F, 1.0F);
 		this.earLeft.setRotationPoint(3.0F, -3.0F, -3.0F);
 		this.head.addChild(this.earLeft);
 		
-		this.body = new ModelRenderer(this, 20, 0);
+		this.body = new ModelRenderer(this, 0, 0);
 		this.body.addBox(-4.0F, -8.0F, -4.0F, 8.0F, 16.0F, 8.0F);
 		this.body.setRotationPoint(0.0F, 12.0F, 0.0F);
 
-		this.tail = new ModelRenderer(this, 0, 15);
-		this.tail.addBox(-1.5F, 0.0F, -3.0F, 3.0F, 8.0F, 3.0F);
+		this.tail = new ModelRenderer(this, 56, 0);
+		this.tail.addBox(-1.0F, 0.0F, -2.0F, 2.0F, 8.0F, 2.0F);
 		this.tail.rotateAngleX = 0.9F;
 		this.tail.setRotationPoint(0.0F, 8.0F, 8.0F);
 
-		this.tail2 = new ModelRenderer(this, 4, 15);
-		this.tail2.addBox(-1.5F, 0.0F, 0.0F, 3.0F, 8.0F, 3.0F);
-		this.tail2.setRotationPoint(0.0F, 20.0F, 14.0F);
+		this.tail2 = new ModelRenderer(this, 56, 10);
+		this.tail2.addBox(-1.0F, 0.0F, 0.0F, 2.0F, 8.0F, 2.0F);
+		this.tail2.setRotationPoint(0.0F, 8.0F, -2.0F);
+		this.tail.addChild(this.tail2);
 
-		this.legBackLeft = new ModelRenderer(this, 8, 13);
+		this.legBackLeft = new ModelRenderer(this, 32, 12);
 		this.legBackLeft.addBox(-1.0F, 0.0F, -3.0F, 3.0F, 8.0F, 3.0F);
 		this.legBackLeft.setRotationPoint(2.0F, 16.0F, 8.0F);
 
-		this.legBackRight = new ModelRenderer(this, 8, 13);
+		this.legBackRight = new ModelRenderer(this, 32, 12);
 		this.legBackRight.addBox(-1.0F, 0.0F, -3.0F, 3.0F, 8.0F, 3.0F);
 		this.legBackRight.setRotationPoint(-3.0F, 16.0F, 8.0F);
 
-		this.legFrontLeft = new ModelRenderer(this, 40, 0);
+		this.legFrontLeft = new ModelRenderer(this, 32, 12);
 		this.legFrontLeft.addBox(-1.0F, 0.0F, 0.0F, 3.0F, 8.0F, 3.0F);
 		this.legFrontLeft.setRotationPoint(2.0F, 16.0F, -8.0F);
 
-		this.legFrontRight = new ModelRenderer(this, 40, 0);
+		this.legFrontRight = new ModelRenderer(this, 32, 12);
 		this.legFrontRight.addBox(-1.0F, 0.0F, 0.0F, 3.0F, 8.0F, 3.0F);
 		this.legFrontRight.setRotationPoint(-3.0F, 16.0F, -8.0F);
 	}
@@ -79,7 +80,7 @@ public class JaguarModel<T extends JaguarEntity> extends AgeableModel<T> {
 	}
 
 	protected Iterable<ModelRenderer> getBodyParts() {
-		return ImmutableList.of(this.body, this.legBackLeft, this.legBackRight, this.legFrontLeft, this.legFrontRight, this.tail, this.tail2);
+		return ImmutableList.of(this.body, this.legBackLeft, this.legBackRight, this.legFrontLeft, this.legFrontRight, this.tail);
 	}
 
 	/**
@@ -93,24 +94,18 @@ public class JaguarModel<T extends JaguarEntity> extends AgeableModel<T> {
 			if (this.state == 2) {
 				this.legBackLeft.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * limbSwingAmount;
 				this.legBackRight.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + 0.3F) * limbSwingAmount;
-				this.legFrontLeft.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float) Math.PI + 0.3F)
-						* limbSwingAmount;
-				this.legFrontRight.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float) Math.PI)
-						* limbSwingAmount;
-				this.tail2.rotateAngleX = 1.7278761F
-						+ ((float) Math.PI / 10F) * MathHelper.cos(limbSwing) * limbSwingAmount;
+				this.legFrontLeft.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float) Math.PI + 0.3F) * limbSwingAmount;
+				this.legFrontRight.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float) Math.PI) * limbSwingAmount;
+				this.tail2.rotateAngleX = 0.7278761F + ((float) Math.PI / 10F) * MathHelper.cos(limbSwing) * limbSwingAmount;
 			} else {
 				this.legBackLeft.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * limbSwingAmount;
-				this.legBackRight.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float) Math.PI)
-						* limbSwingAmount;
-				this.legFrontLeft.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float) Math.PI)
-						* limbSwingAmount;
+				this.legBackRight.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float) Math.PI) * limbSwingAmount;
+				this.legFrontLeft.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float) Math.PI) * limbSwingAmount;
 				this.legFrontRight.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * limbSwingAmount;
 				if (this.state == 1) {
-					this.tail2.rotateAngleX = 1.7278761F
-							+ ((float) Math.PI / 4F) * MathHelper.cos(limbSwing) * limbSwingAmount;
+					this.tail2.rotateAngleX = 0.7278761F + ((float) Math.PI / 4F) * MathHelper.cos(limbSwing) * limbSwingAmount;
 				} else {
-					this.tail2.rotateAngleX = 1.7278761F + 0.47123894F * MathHelper.cos(limbSwing) * limbSwingAmount;
+					this.tail2.rotateAngleX = 0.7278761F + 0.47123894F * MathHelper.cos(limbSwing) * limbSwingAmount;
 				}
 			}
 		}
@@ -120,14 +115,11 @@ public class JaguarModel<T extends JaguarEntity> extends AgeableModel<T> {
 	}
 
 	public void setLivingAnimations(T entityIn, float limbSwing, float limbSwingAmount, float partialTick) {
-		
-		
 		this.tail.rotateAngleX = 0.9F;
 		if (entityIn.isCrouching()) {
 			++this.body.rotationPointY;
 			this.head.rotationPointY += 2.0F;
 			++this.tail.rotationPointY;
-			
 			this.tail.rotateAngleX = ((float) Math.PI / 2F);
 			this.tail2.rotateAngleX = ((float) Math.PI / 2F);
 			this.state = 0;
@@ -139,6 +131,5 @@ public class JaguarModel<T extends JaguarEntity> extends AgeableModel<T> {
 		} else {
 			this.state = 1;
 		}
-
 	}
 }
