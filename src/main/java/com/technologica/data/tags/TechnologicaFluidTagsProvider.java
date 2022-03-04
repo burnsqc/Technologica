7763bd8b2640d9a0f84adb1641f8290cdbce38ac
burnsqc <jonathan.r.mobley@gmail.com>
2022-03-04 05:15:55 -0700

    Update TechnologicaFluidTagsProvider.java

--- src/main/java/com/technologica/data/tags/TechnologicaFluidTagsProvider.java
+++ src/main/java/com/technologica/data/tags/TechnologicaFluidTagsProvider.java
@@ -1,17 +1,12 @@
 package com.technologica.data.tags;
 
-import java.util.Collection;
-import java.util.function.Supplier;
-
 import com.technologica.Technologica;
 import com.technologica.world.level.material.TechnologicaFluids;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.FluidTagsProvider;
 import net.minecraft.tags.FluidTags;
-import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.registries.RegistryObject;
 
 public class TechnologicaFluidTagsProvider extends FluidTagsProvider {
 
@@ -21,21 +16,6 @@ public class TechnologicaFluidTagsProvider extends FluidTagsProvider {
 
 	@Override
 	protected void addTags() {
-		automaticFluidTags(TechnologicaFluids.FLUIDS.getEntries());
-	}
-	
-	/**
-	 * Iterates through a deferred register of fluids, adding entries to tag files based upon translation key content.
-	 * This only works if the translation keys are consistent.  This is really only intended to work for mod fluids which are similar to vanilla.
-	 * This saves a lot of time when adding several new fluids. 
-	 * @param collection a collection of deferred register fluid entries
-	 */
-	
-	private final void automaticFluidTags(Collection<RegistryObject<Fluid>> collection) {
-		for(Supplier<? extends Fluid> fluidSupplier:collection) {
-			Fluid fluid = fluidSupplier.get();
-			
-			if (fluid.getRegistryName().getPath().contains("source") || fluid.getRegistryName().getPath().contains("flowing")) tag(FluidTags.WATER).add(fluid);
-		}
+		this.tag(FluidTags.WATER).add(TechnologicaFluids.HYDROGEN.get(), TechnologicaFluids.FLOWING_HYDROGEN.get(), TechnologicaFluids.HELIUM.get(), TechnologicaFluids.FLOWING_HELIUM.get()); //Add rest of Technologica fluids to this tag
 	}
 }
