e786117615536b212c756e1ddc0c330030234506
burnsqc <jonathan.r.mobley@gmail.com>
2022-03-12 04:58:41 -0700

    Tree placements

--- src/main/java/com/technologica/data/worldgen/TechnologicaVegetationPlacements.java
+++ src/main/java/com/technologica/data/worldgen/TechnologicaVegetationPlacements.java
@@ -5,7 +5,8 @@ import com.technologica.data.worldgen.features.TechnologicaTreeFeatures;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.placement.RarityFilter;
 
 public class TechnologicaVegetationPlacements {
-	public static final PlacedFeature TREES_ZEBRAWOOD = PlacementUtils.register("trees_zebrawood", TechnologicaTreeFeatures.ZEBRAWOOD.placed(VegetationPlacements.treePlacement(PlacementUtils.countExtra(10, 0.1F, 1))));
+	public static final PlacedFeature TREES_ZEBRAWOOD = PlacementUtils.register("trees_zebrawood", TechnologicaTreeFeatures.ZEBRAWOOD.placed(VegetationPlacements.treePlacement(RarityFilter.onAverageOnceEvery(2))));
 }
