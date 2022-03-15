954a046306116242c0e43149e0092818ecc65b9c
NRies60 <97855351+NRies60@users.noreply.github.com>
2022-03-14 20:11:01 -0600

    Update TechnologicaTreePlacements.java

--- src/main/java/com/technologica/data/worldgen/placement/TechnologicaTreePlacements.java
+++ src/main/java/com/technologica/data/worldgen/placement/TechnologicaTreePlacements.java
@@ -7,5 +7,38 @@ import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 public class TechnologicaTreePlacements {
+	public static final PlacedFeature APRICOT_CHECKED = PlacementUtils.register("apricot_checked", TechnologicaTreeFeatures.APRICOT.filteredByBlockSurvival(TechnologicaBlocks.APRICOT_SAPLING.get()));
+	public static final PlacedFeature ASPEN_CHECKED = PlacementUtils.register("aspen_checked", TechnologicaTreeFeatures.ASPEN.filteredByBlockSurvival(TechnologicaBlocks.ASPEN_SAPLING.get()));
+	public static final PlacedFeature AVOCADO_CHECKED = PlacementUtils.register("avocado_checked", TechnologicaTreeFeatures.AVOCADO.filteredByBlockSurvival(TechnologicaBlocks.AVOCADO_SAPLING.get()));
+	public static final PlacedFeature BANANA_CHECKED = PlacementUtils.register("banana_checked", TechnologicaTreeFeatures.BANANA.filteredByBlockSurvival(TechnologicaBlocks.BANANA_SAPLING.get()));
+	public static final PlacedFeature CHERRY_CHECKED = PlacementUtils.register("cherry_checked", TechnologicaTreeFeatures.CHERRY.filteredByBlockSurvival(TechnologicaBlocks.CHERRY_SAPLING.get()));
+	public static final PlacedFeature CHESTNUT_CHECKED = PlacementUtils.register("chestnut_checked", TechnologicaTreeFeatures.CHESTNUT.filteredByBlockSurvival(TechnologicaBlocks.CHESTNUT_SAPLING.get()));
+	public static final PlacedFeature CINNAMON_CHECKED = PlacementUtils.register("cinnamon_checked", TechnologicaTreeFeatures.CINNAMON.filteredByBlockSurvival(TechnologicaBlocks.CINNAMON_SAPLING.get()));
+	public static final PlacedFeature COCONUT_CHECKED = PlacementUtils.register("coconut_checked", TechnologicaTreeFeatures.COCONUT.filteredByBlockSurvival(TechnologicaBlocks.COCONUT_SAPLING.get()));
+	public static final PlacedFeature EBONY_CHECKED = PlacementUtils.register("ebony_checked", TechnologicaTreeFeatures.EBONY.filteredByBlockSurvival(TechnologicaBlocks.EBONY_SAPLING.get()));
+	public static final PlacedFeature KIWI_CHECKED = PlacementUtils.register("kiwi_checked", TechnologicaTreeFeatures.KIWI.filteredByBlockSurvival(TechnologicaBlocks.KIWI_SAPLING.get()));
+	public static final PlacedFeature LEMON_CHECKED = PlacementUtils.register("lemon_checked", TechnologicaTreeFeatures.LEMON.filteredByBlockSurvival(TechnologicaBlocks.LEMON_SAPLING.get()));
+	public static final PlacedFeature LIME_CHECKED = PlacementUtils.register("lime_checked", TechnologicaTreeFeatures.LIME.filteredByBlockSurvival(TechnologicaBlocks.LIME_SAPLING.get()));
+	public static final PlacedFeature MAHOGANY_CHECKED = PlacementUtils.register("mahogany_checked", TechnologicaTreeFeatures.MAHOGANY.filteredByBlockSurvival(TechnologicaBlocks.MAHOGANY_SAPLING.get()));
+	public static final PlacedFeature MAPLE_CHECKED = PlacementUtils.register("maple_checked", TechnologicaTreeFeatures.MAPLE.filteredByBlockSurvival(TechnologicaBlocks.MAPLE_SAPLING.get()));
+	public static final PlacedFeature OLIVE_CHECKED = PlacementUtils.register("olive_checked", TechnologicaTreeFeatures.OLIVE.filteredByBlockSurvival(TechnologicaBlocks.OLIVE_SAPLING.get()));
+	public static final PlacedFeature ORANGE_CHECKED = PlacementUtils.register("orange_checked", TechnologicaTreeFeatures.ORANGE.filteredByBlockSurvival(TechnologicaBlocks.ORANGE_SAPLING.get()));
+	public static final PlacedFeature PEACH_CHECKED = PlacementUtils.register("peach_checked", TechnologicaTreeFeatures.PEACH.filteredByBlockSurvival(TechnologicaBlocks.PEACH_SAPLING.get()));
+	public static final PlacedFeature PEAR_CHECKED = PlacementUtils.register("pear_checked", TechnologicaTreeFeatures.PEAR.filteredByBlockSurvival(TechnologicaBlocks.PEAR_SAPLING.get()));
+	public static final PlacedFeature PLUM_CHECKED = PlacementUtils.register("pulm_checked", TechnologicaTreeFeatures.PLUM.filteredByBlockSurvival(TechnologicaBlocks.PLUM_SAPLING.get()));
+	public static final PlacedFeature REDWOOD_CHECKED = PlacementUtils.register("redwood_checked", TechnologicaTreeFeatures.REDWOOD.filteredByBlockSurvival(TechnologicaBlocks.REDWOOD_SAPLING.get()));
+	public static final PlacedFeature ROSEWOOD_CHECKED = PlacementUtils.register("rosewood_checked", TechnologicaTreeFeatures.ROSEWOOD.filteredByBlockSurvival(TechnologicaBlocks.ROSEWOOD_SAPLING.get()));
+	public static final PlacedFeature RUBBER_CHECKED = PlacementUtils.register("rubber_checked", TechnologicaTreeFeatures.RUBBER.filteredByBlockSurvival(TechnologicaBlocks.RUBBER_SAPLING.get()));
+	public static final PlacedFeature TEAK_CHECKED = PlacementUtils.register("teak_checked", TechnologicaTreeFeatures.TEAK.filteredByBlockSurvival(TechnologicaBlocks.TEAK_SAPLING.get()));
+	public static final PlacedFeature WALNUT_CHECKED = PlacementUtils.register("walnut_checked", TechnologicaTreeFeatures.WALNUT.filteredByBlockSurvival(TechnologicaBlocks.WALNUT_SAPLING.get()));
 	public static final PlacedFeature ZEBRAWOOD_CHECKED = PlacementUtils.register("zebrawood_checked", TechnologicaTreeFeatures.ZEBRAWOOD.filteredByBlockSurvival(TechnologicaBlocks.ZEBRAWOOD_SAPLING.get()));
+	
+	public static final PlacedFeature ANCIENT_AMBROSIA_CHECKED = PlacementUtils.register("ancient_ambrosia_checked", TechnologicaTreeFeatures.ANCIENT_AMBROSIA.filteredByBlockSurvival(TechnologicaBlocks.ANCIENT_AMBROSIA_SAPLING.get()));
+	public static final PlacedFeature BENEVOLENT_APOTHECARY_CHECKED = PlacementUtils.register("benevolent_apothecary_checked", TechnologicaTreeFeatures.BENEVOLENT_APOTHECARY.filteredByBlockSurvival(TechnologicaBlocks.BENEVOLENT_APOTHECARY_SAPLING.get()));
+	public static final PlacedFeature CRYOGENIC_SPIRE_CHECKED = PlacementUtils.register("cryogenic_spire_checked", TechnologicaTreeFeatures.CRYOGENIC_SPIRE.filteredByBlockSurvival(TechnologicaBlocks.CRYOGENIC_SPIRE_SAPLING.get()));
+	public static final PlacedFeature MALEVOLENT_APOTHECARY_CHECKED = PlacementUtils.register("malevolent_apothecary_checked", TechnologicaTreeFeatures.MALEVOLENT_APOTHECARY.filteredByBlockSurvival(TechnologicaBlocks.MALEVOLENT_APOTHECARY_SAPLING.get()));
+	public static final PlacedFeature NECROTIC_CHECKED = PlacementUtils.register("necrotic_checked", TechnologicaTreeFeatures.NECROTIC.filteredByBlockSurvival(TechnologicaBlocks.NECROTIC_SAPLING.get()));
+	public static final PlacedFeature SERENDIPITOUS_APOTHECARY_CHECKED = PlacementUtils.register("serendipitous_apothecary_checked", TechnologicaTreeFeatures.SERENDIPITOUS_APOTHECARY.filteredByBlockSurvival(TechnologicaBlocks.SERENDIPITOUS_APOTHECARY_SAPLING.get()));
+	public static final PlacedFeature THUNDEROUS_CONDUCTOR_CHECKED = PlacementUtils.register("thunderous_conductor_checked", TechnologicaTreeFeatures.THUNDEROUS_CONDUCTOR.filteredByBlockSurvival(TechnologicaBlocks.THUNDEROUS_CONDUCTOR_SAPLING.get()));
+	public static final PlacedFeature TOWERING_INFERNO_CHECKED = PlacementUtils.register("towering_inferno_checked", TechnologicaTreeFeatures.TOWERING_INFERNO.filteredByBlockSurvival(TechnologicaBlocks.TOWERING_INFERNO_SAPLING.get()));
 }
