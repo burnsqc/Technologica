50a36f1d6cf72ecc92ac551292b85efd90189017
burnsqc <jonathan.r.mobley@gmail.com>
2022-03-23 20:08:53 -0600

    Run data Mahogany/Maple Signs

---
+++ src/main/java/com/technologica/data/loot/TechnologicaEntityLoot.java
@@ -0,0 +1,35 @@
+package com.technologica.data.loot;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.technologica.Technologica;
+import com.technologica.world.entity.TechnologicaEntityType;
+
+import net.minecraft.data.loot.EntityLoot;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.LootingEnchantFunction;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.functions.SmeltItemFunction;
+import net.minecraft.world.level.storage.loot.predicates.LootItemEntityPropertyCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class TechnologicaEntityLoot extends EntityLoot {
+	
+	@Override
+	protected void addTables() {
+		add(TechnologicaEntityType.DUCK.get(),LootTable.lootTable().withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F)).add(LootItem.lootTableItem(Items.FEATHER).apply(SetItemCountFunction.setCount(UniformGenerator.between(0.0F, 2.0F))).apply(LootingEnchantFunction.lootingMultiplier(UniformGenerator.between(0.0F, 1.0F))))).withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F)).add(LootItem.lootTableItem(Items.CHICKEN).apply(SmeltItemFunction.smelted().when(LootItemEntityPropertyCondition.hasProperties(LootContext.EntityTarget.THIS, ENTITY_ON_FIRE))).apply(LootingEnchantFunction.lootingMultiplier(UniformGenerator.between(0.0F, 1.0F))))));
+	}
+	
+	@Override
+	protected Iterable<EntityType<?>> getKnownEntities() {
+		return ForgeRegistries.ENTITIES.getValues().stream().filter(entity -> Optional.ofNullable(entity.getRegistryName()).filter(registryName -> registryName.getNamespace().equals(Technologica.MODID)).isPresent()).collect(Collectors.toList());
+	}
+}
