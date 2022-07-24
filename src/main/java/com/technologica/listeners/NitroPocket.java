826b4c7fab78357993a383fe1312a2b1ebf6907b
NRies60 <97855351+NRies60@users.noreply.github.com>
2022-07-24 12:58:42 -0600

    Nitro Texture and listener

--- src/main/java/com/technologica/listeners/NitroPocket.java
+++ src/main/java/com/technologica/listeners/NitroPocket.java
@@ -3,14 +3,14 @@ package com.technologica.listeners;
 import com.technologica.world.item.TechnologicaItems;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
-import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class NitroPocket {
@@ -27,8 +27,7 @@ public class NitroPocket {
 					ItemStack testStack = player.getInventory().getItem(i);
 					if (testStack.getItem() == TechnologicaItems.NITRO_ITEM.get()) {
 						if (level.getRandom().nextBoolean()) {
-							level.explode(null, player.getX(), player.getY(), player.getZ(), 4.0F,
-									Explosion.BlockInteraction.BREAK);
+							level.explode(null, player.getX(), player.getY(), player.getZ(), 4.0F, Explosion.BlockInteraction.BREAK);
 						}
 					}
 				}
@@ -37,9 +36,22 @@ public class NitroPocket {
 	}
 
 	@SubscribeEvent
-	public void onLivingFallEvent(LivingFallEvent event) {
-		if (event.getEntity().level.dimension().toString().contains("technologica:moon")) {
-			event.setDamageMultiplier(0F);
+	public void onLivingDamageEvent(LivingDamageEvent event) {
+		if (event.getEntity() instanceof Player) {
+			Player player = (Player) event.getEntity();
+			Level level = player.getLevel();
+			for (int i = 0; i < player.getInventory().getContainerSize(); ++i) {
+				ItemStack testStack = player.getInventory().getItem(i);
+				if (testStack.getItem() == TechnologicaItems.NITRO_ITEM.get()) {
+					if (event.getSource() == DamageSource.IN_FIRE || event.getSource() == DamageSource.ON_FIRE || event.getSource() == DamageSource.LAVA || event.getSource() == DamageSource.LIGHTNING_BOLT || event.getSource() == DamageSource.HOT_FLOOR || event.getSource().isExplosion()) {
+						player.getInventory().removeItem(testStack);
+						level.explode(null, player.getX(), player.getY(), player.getZ(), 4.0F, Explosion.BlockInteraction.BREAK);
+					} else if (level.getRandom().nextBoolean()) {
+						player.getInventory().removeItem(testStack);
+						level.explode(null, player.getX(), player.getY(), player.getZ(), 4.0F, Explosion.BlockInteraction.BREAK);
+					}
+				}
+			}
 		}
 	}
 }
