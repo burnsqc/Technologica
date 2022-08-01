6f05183a9b1d12b933ac2afcf30b31abb13211a7
burnsqc <jonathan.r.mobley@gmail.com>
2022-07-31 21:47:24 -0600

    Land Mine BETicker

--- src/main/java/com/technologica/world/level/block/LandMineBlock.java
+++ src/main/java/com/technologica/world/level/block/LandMineBlock.java
@@ -3,7 +3,6 @@ package com.technologica.world.level.block;
 import javax.annotation.Nullable;
 
 import com.technologica.world.level.block.entity.LandMineBlockEntity;
-import com.technologica.world.level.block.entity.TechnologicaBlockEntityType;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
@@ -11,8 +10,8 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
@@ -24,14 +23,12 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.Material;
 
-public class LandMineBlock extends Block implements EntityBlock {
-	public static final IntegerProperty MULCH_AGE = BlockStateProperties.AGE_3; // Change for arming time
+public class LandMineBlock extends BaseEntityBlock {
+	public static final IntegerProperty MULCH_AGE = BlockStateProperties.AGE_3;
 
 	public LandMineBlock() {
-		super(BlockBehaviour.Properties.of(Material.GRASS).randomTicks().strength(0.6F).sound(SoundType.GRASS)); // Change
-																													// Sound
-		this.registerDefaultState(this.stateDefinition.any().setValue(MULCH_AGE, 0)); // Change for mine arming time?
-
+		super(BlockBehaviour.Properties.of(Material.GRASS).randomTicks().strength(0.6F).sound(SoundType.GRASS));
+		this.registerDefaultState(this.stateDefinition.any().setValue(MULCH_AGE, 0));
 	}
 
 	/*
@@ -41,20 +38,20 @@ public class LandMineBlock extends Block implements EntityBlock {
 	public LandMineBlockEntity getTileEntity(Level worldIn, BlockPos posIn) {
 		return (LandMineBlockEntity) worldIn.getBlockEntity(posIn);
 	}
-	
-    @Override
+
+	@Override
 	public void stepOn(Level p_153777_, BlockPos p_153778_, BlockState p_153779_, Entity p_153780_) {
-    	LandMineBlockEntity landMineBlockEntity = getTileEntity(p_153777_, p_153778_);
-    	if(!landMineBlockEntity.getArmed()) {
-	    	landMineBlockEntity.setArmed();
-	    	p_153777_.playSound((Player) null, p_153778_, SoundEvents.ANVIL_HIT, SoundSource.BLOCKS, 1.0F, 0.8F + p_153777_.random.nextFloat() * 0.4F);
-    	}
+		LandMineBlockEntity landMineBlockEntity = getTileEntity(p_153777_, p_153778_);
+		if (!landMineBlockEntity.getArmed()) {
+			landMineBlockEntity.setArmed();
+			p_153777_.playSound((Player) null, p_153778_, SoundEvents.ANVIL_HIT, SoundSource.BLOCKS, 1.0F, 0.8F + p_153777_.random.nextFloat() * 0.4F);
+		}
 		super.stepOn(p_153777_, p_153778_, p_153779_, p_153780_);
 	}
 
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builderIn) {
-		builderIn.add(MULCH_AGE); // Change for mine arming?
+		builderIn.add(MULCH_AGE);
 		super.createBlockStateDefinition(builderIn);
 	}
 
@@ -64,12 +61,19 @@ public class LandMineBlock extends Block implements EntityBlock {
 
 	@Override
 	public BlockEntity newBlockEntity(BlockPos p_153215_, BlockState p_153216_) {
-		return new LandMineBlockEntity(p_153215_, p_153216_); 
+		return new LandMineBlockEntity(p_153215_, p_153216_);
 	}
-	
+
 	@Override
 	@Nullable
 	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState, BlockEntityType<T> type) {
-		return level.isClientSide() ? null : createTickHelper(type, TechnologicaBlockEntityType.LAND_MINE_TILE.get(), LandMineBlockEntity::serverTick);
+		if (level.isClientSide()) {
+			return null;
+		}
+		return (lvl, pos, blockState2, t) -> {
+			if (t instanceof LandMineBlockEntity tile) {
+				tile.serverTick();
+			}
+		};
 	}
 }
--- src/main/java/com/technologica/world/level/block/entity/LandMineBlockEntity.java
+++ src/main/java/com/technologica/world/level/block/entity/LandMineBlockEntity.java
@@ -11,51 +11,52 @@ import net.minecraft.world.level.block.state.BlockState;
 public class LandMineBlockEntity extends BlockEntity {
 	private BlockState previousBlockState = Blocks.AIR.defaultBlockState();
 	private boolean isArmed = false;
-	
+
 	public LandMineBlockEntity(BlockPos p_155700_, BlockState p_155701_) {
 		super(TechnologicaBlockEntityType.LAND_MINE_TILE.get(), p_155700_, p_155701_);
 	}
 
 	public BlockState getPreviousBlockState() {
-        return previousBlockState;
-    }
-	
+		return previousBlockState;
+	}
+
 	public void setPreviousBlockState(BlockState previousBlockStateIn) {
-        this.previousBlockState = previousBlockStateIn;
-        setChanged();
-        if (level != null) {
-        	level.sendBlockUpdated(worldPosition, getBlockState(), getBlockState(), 2); 
-        }
-    }
-	
+		this.previousBlockState = previousBlockStateIn;
+		setChanged();
+		if (level != null) {
+			level.sendBlockUpdated(worldPosition, getBlockState(), getBlockState(), 2);
+		}
+	}
+
 	public Boolean getArmed() {
-        return isArmed;
-    }
-	
+		return isArmed;
+	}
+
 	public void setArmed() {
-       isArmed = true;
-    }
-	
-	public void tick() {
+		isArmed = true;
+	}
+
+	public void serverTick() {
 		if (isArmed) {
 			assert level != null;
-			if (!level.hasNearbyAlivePlayer((double)worldPosition.getX() + 0.5D, (double)worldPosition.getY() + 0.5D, (double)worldPosition.getZ() + 0.5D, 1.0D)) {
+			if (!level.hasNearbyAlivePlayer(worldPosition.getX() + 0.5D, worldPosition.getY() + 0.5D, worldPosition.getZ() + 0.5D, 1.0D)) {
 				level.explode(null, worldPosition.getX(), worldPosition.getY(), worldPosition.getZ(), 4.0F, Explosion.BlockInteraction.BREAK);
-			}	
+			}
 		}
-		
+
 	}
+
 	@Override
 	public void load(CompoundTag nbt) {
 		super.load(nbt);
-	    if (nbt.contains("previousState")) {
-	    	this.setPreviousBlockState(NbtUtils.readBlockState(nbt.getCompound("previousState")));
-	    }
+		if (nbt.contains("previousState")) {
+			this.setPreviousBlockState(NbtUtils.readBlockState(nbt.getCompound("previousState")));
+		}
 	}
 
 	@Override
 	public void saveAdditional(CompoundTag compound) {
 		super.saveAdditional(compound);
-	    compound.put("previousState", NbtUtils.writeBlockState(getPreviousBlockState())); 			    
+		compound.put("previousState", NbtUtils.writeBlockState(getPreviousBlockState()));
 	}
 }
