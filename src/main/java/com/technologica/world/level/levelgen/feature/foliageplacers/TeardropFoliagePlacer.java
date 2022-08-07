180f2bb66b8277112df4daf391f7ed3915b7aa60
burnsqc <jonathan.r.mobley@gmail.com>
2022-08-07 09:20:53 -0600

    Update TeardropFoliagePlacer.java

--- src/main/java/com/technologica/world/level/levelgen/feature/foliageplacers/TeardropFoliagePlacer.java
+++ src/main/java/com/technologica/world/level/levelgen/feature/foliageplacers/TeardropFoliagePlacer.java
@@ -19,8 +19,8 @@ import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacerTy
 
 public class TeardropFoliagePlacer extends FoliagePlacer {
 	public static final Codec<TeardropFoliagePlacer> teardropCodec = RecordCodecBuilder.create(p_236742_0_ -> blobParts(p_236742_0_).apply(p_236742_0_, TeardropFoliagePlacer::new));
-	
-	protected final int layersBelowTop;
+
+	protected int layersBelowTop;
 	protected boolean largeTree;
 
 	protected static <P extends TeardropFoliagePlacer> P3<Mu<P>, IntProvider, IntProvider, Integer> blobParts(Instance<P> p_236740_0_) {
@@ -39,10 +39,10 @@ public class TeardropFoliagePlacer extends FoliagePlacer {
 	}
 
 	@Override
-	protected void createFoliage(LevelSimulatedReader p_161422_, BiConsumer<BlockPos, BlockState> p_161423_, Random p_161424_, TreeConfiguration p_161425_, int p_161426_, FoliageAttachment p_161427_, int p_161428_, int diameter, int topLayer) {
-		for (int layer = topLayer + 3; layer >= topLayer + 3 - layersBelowTop; --layer) {
-			int j = Math.max(diameter + 1 + p_161427_.radiusOffset(), 0);
-			this.placeLeavesRow(p_161422_, p_161423_, p_161424_, p_161425_, p_161427_.pos(), j, layer, p_161427_.doubleTrunk());
+	protected void createFoliage(LevelSimulatedReader readerIn, BiConsumer<BlockPos, BlockState> biConsumerIn, Random randomIn, TreeConfiguration configIn, int p_161426_, FoliageAttachment attachmentIn, int p_161428_, int diameter, int topLayer) {
+		for (int layer = topLayer + 3; layer >= topLayer + 3 - p_161428_; --layer) {
+			int j = Math.max(diameter + 1 + attachmentIn.radiusOffset(), 0);
+			this.placeLeavesRow(readerIn, biConsumerIn, randomIn, configIn, attachmentIn.pos(), j, layer, attachmentIn.doubleTrunk());
 		}
 	}
 
@@ -50,8 +50,9 @@ public class TeardropFoliagePlacer extends FoliagePlacer {
 	public int foliageHeight(Random randomIn, int i, TreeConfiguration configIn) {
 		int trim;
 		if (i == 7) {
-			trim = i+1;
+			trim = i + 1;
 			largeTree = true;
+
 		} else {
 			trim = layersBelowTop;
 			largeTree = false;
@@ -61,28 +62,28 @@ public class TeardropFoliagePlacer extends FoliagePlacer {
 
 	@Override
 	protected boolean shouldSkipLocation(Random randomIn, int relativeZ, int relativeY, int relativeX, int p_230373_5_, boolean p_230373_6_) {
-		if (relativeY>=2) {
+		if (relativeY >= 2) {
 			return relativeX + relativeZ >= 1;
-		} else if (relativeY==1) {
+		} else if (relativeY == 1) {
 			return relativeX + relativeZ >= 2;
-		} else if (relativeY==0) { 
+		} else if (relativeY == 0) {
 			return relativeX >= 2 || relativeZ >= 2;
-		} else if (relativeY==-1) { 
+		} else if (relativeY == -1) {
 			return relativeX + relativeZ >= 3;
-		} else if (relativeY==-2) { 
+		} else if (relativeY == -2) {
 			return relativeX + relativeZ >= 4 || relativeX >= 3 || relativeZ >= 3;
-		} else if (relativeY==-3) {
+		} else if (relativeY == -3) {
 			if (largeTree) {
 				return relativeX + relativeZ >= 5;
 			} else {
 				return relativeX >= 2 || relativeZ >= 2;
 			}
-		} else if (relativeY==-4) {
+		} else if (relativeY == -4) {
 			return relativeX + relativeZ >= 4 || relativeX >= 3 || relativeZ >= 3;
-		} else if (relativeY==-5) {
+		} else if (relativeY == -5) {
 			return relativeX >= 2 || relativeZ >= 2;
 		} else {
 			return false;
-		}		
+		}
 	}
 }
