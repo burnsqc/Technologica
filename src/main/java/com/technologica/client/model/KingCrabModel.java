9f32c2e59e32c61738d80ddc0e91cf0cd65248d2
NRies60 <97855351+NRies60@users.noreply.github.com>
2022-09-24 08:01:11 -0600

    King Crab

--- src/main/java/com/technologica/client/model/KingCrabModel.java
+++ src/main/java/com/technologica/client/model/KingCrabModel.java
@@ -1,5 +1,7 @@
 package com.technologica.client.model;
 
+import com.google.common.collect.ImmutableList;
+
 import net.minecraft.client.model.ListModel;
 import net.minecraft.client.model.QuadrupedModel;
 import net.minecraft.client.model.geom.ModelPart;
@@ -12,7 +14,7 @@ import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.world.entity.Entity;
 
 public class KingCrabModel<T extends Entity> extends ListModel<T> {
-	/*
+
 	private final ModelPart body;
 	private final ModelPart head;
 	
@@ -39,157 +41,79 @@ public class KingCrabModel<T extends Entity> extends ListModel<T> {
 	private final ModelPart clawRight;
 	private final ModelPart pincherLeft;
 	private final ModelPart pincherRight;
-	*/
 	
-	public KingCrabModel() {
-		/*
-		this.texWidth = 64;
-		this.texHeight = 32;
+	public KingCrabModel(ModelPart modelPartIn) {
 
-		this.body = new ModelPart(this, 0, 0);
-		this.body.setPos(0.0F, 6.0F, 0.0F);
-		this.body.addBox(-4.0F, -2.0F, -3.0F, 8.0F, 4.0F, 7.0F);
+		this.body = modelPartIn.getChild("body");
+		this.head = body.getChild("head");
+		this.legBackBackLeftUpper = modelPartIn.getChild("legBackBackLeftUpper");
+		this.legBackBackLeftLower = legBackBackLeftUpper.getChild("legBackBackLeftLower");
+		this.legBackBackRightUpper = modelPartIn.getChild("legBackBackRightUpper");
+		this.legBackBackRightLower = legBackBackRightUpper.getChild("legBackBackRightLower");
+		this.legBackLeftUpper = modelPartIn.getChild("legBackLeftUpper");
+		this.legBackLeftLower = legBackLeftUpper.getChild("legBackLeftLower");
+		this.legBackRightUpper = modelPartIn.getChild("legBackRightUpper");
+		this.legBackRightLower = legBackRightUpper.getChild("legBackRightLower");
+		this.legFrontLeftUpper = modelPartIn.getChild("legFrontLeftUpper");
+		this.legFrontLeftLower = legFrontLeftUpper.getChild("legFrontLeftLower");
+		this.legFrontRightUpper = modelPartIn.getChild("legFrontRightUpper");
+		this.legFrontRightLower = legFrontRightUpper.getChild("legFrontRightLower");
+		this.legFrontFrontLeftUpper = modelPartIn.getChild("legFrontFrontLeftUpper");
+		this.legFrontFrontLeftLower = legFrontFrontLeftUpper.getChild("legFrontFrontLeftLower");
+		this.legFrontFrontRightUpper = modelPartIn.getChild("legFrontFrontRightUpper");
+		this.legFrontFrontRightLower = legFrontFrontRightUpper.getChild("legFrontFrontRightLower");
+		this.armLeft = body.getChild("armLeft");
+		this.clawLeft = armLeft.getChild("clawLeft");
+		this.pincherLeft = clawLeft.getChild("pincherLeft");
+		this.armRight = body.getChild("armRight");
+		this.clawRight = armRight.getChild("clawRight");
+		this.pincherRight = clawRight.getChild("pincherRight");
 		
-		this.head = new ModelPart(this, 0, 11);
-		this.head.setPos(0.0F, -1.0F, -3.0F);
-		this.head.addBox(-2.0F, -1.0F, -2.0F, 4.0F, 2.0F, 4.0F);
-		this.body.addChild(this.head);
-		
-		this.legBackBackLeftUpper = new ModelPart(this, 0, 17);
-		this.legBackBackLeftUpper.setPos(4.0F, 7.0F, 2.0F);
-		this.legBackBackLeftUpper.addBox(-1.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-		
-		this.legBackBackLeftLower = new ModelPart(this, 0, 17);
-		this.legBackBackLeftLower.setPos(15.0F, 1.0F, 0.0F);
-		this.legBackBackLeftLower.addBox(0.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-		this.legBackBackLeftUpper.addChild(this.legBackBackLeftLower);
-		
-		this.legBackBackRightUpper = new ModelPart(this, 0, 17);
-		this.legBackBackRightUpper.setPos(-4.0F, 7.0F, 2.0F);
-		this.legBackBackRightUpper.addBox(-15.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-		
-		this.legBackBackRightLower = new ModelPart(this, 0, 17);
-		this.legBackBackRightLower.setPos(-15.0F, 1.0F, 0.0F);
-		this.legBackBackRightLower.addBox(-16.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-		this.legBackBackRightUpper.addChild(this.legBackBackRightLower);
-		
-		this.legBackLeftUpper = new ModelPart(this, 0, 17);
-		this.legBackLeftUpper.setPos(4.0F, 7.0F, 1.0F);
-		this.legBackLeftUpper.addBox(-1.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-		
-		this.legBackLeftLower = new ModelPart(this, 0, 17);
-		this.legBackLeftLower.setPos(15.0F, 1.0F, 0.0F);
-		this.legBackLeftLower.addBox(0.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-		this.legBackLeftUpper.addChild(this.legBackLeftLower);
-		
-		this.legBackRightUpper = new ModelPart(this, 0, 17);
-		this.legBackRightUpper.setPos(-4.0F, 7.0F, 1.0F);
-		this.legBackRightUpper.addBox(-15.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-
-		this.legBackRightLower = new ModelPart(this, 0, 17);
-		this.legBackRightLower.setPos(-15.0F, 1.0F, 0.0F);
-		this.legBackRightLower.addBox(-16.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-		this.legBackRightUpper.addChild(this.legBackRightLower);
-
-		this.legFrontLeftUpper = new ModelPart(this, 0, 17);
-		this.legFrontLeftUpper.setPos(4.0F, 7.0F, 0.0F);
-		this.legFrontLeftUpper.addBox(-1.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-		
-		this.legFrontLeftLower = new ModelPart(this, 0, 17);
-		this.legFrontLeftLower.setPos(15.0F, 1.0F, 0.0F);
-		this.legFrontLeftLower.addBox(0.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-		this.legFrontLeftUpper.addChild(this.legFrontLeftLower);
-		
-		this.legFrontRightUpper = new ModelPart(this, 0, 17);
-		this.legFrontRightUpper.setPos(-4.0F, 7.0F, 0.0F);
-		this.legFrontRightUpper.addBox(-15.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-
-		this.legFrontRightLower = new ModelPart(this, 0, 17);
-		this.legFrontRightLower.setPos(-15.0F, 1.0F, 0.0F);
-		this.legFrontRightLower.addBox(-16.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-		this.legFrontRightUpper.addChild(this.legFrontRightLower);
-
-		this.legFrontFrontLeftUpper = new ModelPart(this, 0, 17);
-		this.legFrontFrontLeftUpper.setPos(4.0F, 7.0F, -1.0F);
-		this.legFrontFrontLeftUpper.addBox(-1.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-		
-		this.legFrontFrontLeftLower = new ModelPart(this, 0, 17);
-		this.legFrontFrontLeftLower.setPos(15.0F, 1.0F, 0.0F);
-		this.legFrontFrontLeftLower.addBox(0.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-		this.legFrontFrontLeftUpper.addChild(this.legFrontFrontLeftLower);
-		
-		this.legFrontFrontRightUpper = new ModelPart(this, 0, 17);
-		this.legFrontFrontRightUpper.setPos(-4.0F, 7.0F, -1.0F);
-		this.legFrontFrontRightUpper.addBox(-15.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-
-		this.legFrontFrontRightLower = new ModelPart(this, 0, 17);
-		this.legFrontFrontRightLower.setPos(-15.0F, 1.0F, 0.0F);
-		this.legFrontFrontRightLower.addBox(-16.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F);
-		this.legFrontFrontRightUpper.addChild(this.legFrontFrontRightLower);
-		
-		this.armLeft = new ModelPart(this, 0, 21);
-		this.armLeft.setPos(4.0F, 1.0F, -3.0F);
-		this.armLeft.addBox(-2.0F, -1.0F, -1.0F, 10.0F, 2.0F, 2.0F);
-		this.body.addChild(this.armLeft);
-		
-		this.armRight = new ModelPart(this, 0, 21);
-		this.armRight.setPos(-4.0F, 1.0F, -3.0F);
-		this.armRight.addBox(-8.0F, -1.0F, -1.0F, 10.0F, 2.0F, 2.0F);
-		this.body.addChild(this.armRight);
-
-		this.clawLeft = new ModelPart(this, 0, 21);
-		this.clawLeft.setPos(8.0F, 0.0F, 1.0F);
-		this.clawLeft.addBox(0.0F, -1.0F, -2.0F, 10.0F, 2.0F, 2.0F);
-		this.armLeft.addChild(this.clawLeft);
-		
-		this.clawRight = new ModelPart(this, 0, 21);
-		this.clawRight.setPos(-8.0F, 0.0F, 1.0F);
-		this.clawRight.addBox(0.0F, -1.0F, 0.0F, 10.0F, 2.0F, 2.0F);
-		this.armRight.addChild(this.clawRight);
-		
-		this.pincherLeft = new ModelPart(this, 0, 25);
-		this.pincherLeft.setPos(5.0F, 0.0F, 0.0F);
-		this.pincherLeft.addBox(0.0F, -1.0F, -2.0F, 5.0F, 2.0F, 2.0F);
-		this.clawLeft.addChild(this.pincherLeft);
-
-		this.pincherRight = new ModelPart(this, 0, 25);
-		this.pincherRight.setPos(5.0F, 0.0F, 0.0F);
-		this.pincherRight.addBox(0.0F, -1.0F, 0.0F, 5.0F, 2.0F, 2.0F);
-		this.pincherRight.mirror = true;
-		this.clawRight.addChild(this.pincherRight);
-		*/
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = QuadrupedModel.createBodyMesh(6, CubeDeformation.NONE);
 		PartDefinition partdefinition = meshdefinition.getRoot();
 		
-		partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 14).addBox(-2.0F, -4.0F, -4.0F, 4.0F, 4.0F, 4.0F), PartPose.offset(0.0F, 21.0F, -4.0F));
-		partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, 10.0F, -6.0F, 6.0F, 6.0F, 8.0F), PartPose.offset(0.0F, 6.0F, 2.0F));
+		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -2.0F, -3.0F, 8.0F, 4.0F, 7.0F), PartPose.offset(0.0F, 6.0F, 0.0F));
 		
-		partdefinition.addOrReplaceChild("rightHindLeg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F), PartPose.offset(-2.0F, 21.0F, 3.0F));
-		partdefinition.addOrReplaceChild("leftHindLeg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F), PartPose.offset(2.0F, 21.0F, 3.0F));
-		partdefinition.addOrReplaceChild("rightFrontLeg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F), PartPose.offset(-2.0F, 21.0F, -3.0F));
-		partdefinition.addOrReplaceChild("leftFrontLeg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F), PartPose.offset(2.0F, 21.0F, -3.0F));
-		partdefinition.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(0, 22).addBox(-2.0F, 0.0F, 0.0F, 4.0F, 1.0F, 6.0F), PartPose.offset(0.0F, 14.0F, 2.0F));
+		PartDefinition head = body.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 11).addBox(-2.0F, -1.0F, -2.0F, 4.0F, 2.0F, 4.0F), PartPose.offset(0.0F, -1.0F, -3.0F));
 		
-		return LayerDefinition.create(meshdefinition, 32, 32);
+		PartDefinition legBackBackLeftUpper = partdefinition.addOrReplaceChild("legBackBackLeftUpper", CubeListBuilder.create().texOffs(0, 17).addBox(-1.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(4.0F, 7.0F, 2.0F));
+		legBackBackLeftUpper.addOrReplaceChild("legBackBackLeftLower", CubeListBuilder.create().texOffs(0, 17).addBox(0.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(15.0F, 1.0F, 0.0F));
+		
+		PartDefinition legBackBackRightUpper = partdefinition.addOrReplaceChild("legBackBackRightUpper", CubeListBuilder.create().texOffs(0, 17).addBox(-15.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(-4.0F, 7.0F, 2.0F));
+		legBackBackRightUpper.addOrReplaceChild("legBackBackRightLower", CubeListBuilder.create().texOffs(0, 17).addBox(-16.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(-15.0F, 1.0F, 0.0F));
+		
+		PartDefinition legBackLeftUpper = partdefinition.addOrReplaceChild("legBackLeftUpper", CubeListBuilder.create().texOffs(0, 17).addBox(-1.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(4.0F, 7.0F, 1.0F));
+		legBackLeftUpper.addOrReplaceChild("legBackLeftLower", CubeListBuilder.create().texOffs(0, 17).addBox(0.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(15.0F, 1.0F, 0.0F));
+		
+		PartDefinition legBackRightUpper = partdefinition.addOrReplaceChild("legBackRightUpper", CubeListBuilder.create().texOffs(0, 17).addBox(-15.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(-4.0F, 7.0F, 1.0F));
+		legBackRightUpper.addOrReplaceChild("legBackRightLower", CubeListBuilder.create().texOffs(0, 17).addBox(-16.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(-15.0F, 1.0F, 0.0F));
+		
+		PartDefinition legFrontLeftUpper = partdefinition.addOrReplaceChild("legFrontLeftUpper", CubeListBuilder.create().texOffs(0, 17).addBox(-1.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(4.0F, 7.0F, 0.0F));
+		legFrontLeftUpper.addOrReplaceChild("legFrontLeftLower", CubeListBuilder.create().texOffs(0, 17).addBox(0.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(15.0F, 1.0F, 0.0F));
+		
+		PartDefinition legFrontRightUpper = partdefinition.addOrReplaceChild("legFrontRightUpper", CubeListBuilder.create().texOffs(0, 17).addBox(-15.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(-4.0F, 7.0F, 0.0F));
+		legFrontRightUpper.addOrReplaceChild("legFrontRightLower", CubeListBuilder.create().texOffs(0, 17).addBox(-16.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(-15.0F, 1.0F, 0.0F));
+		
+		PartDefinition legFrontFrontLeftUpper = partdefinition.addOrReplaceChild("legFrontFrontLeftUpper", CubeListBuilder.create().texOffs(0, 17).addBox(-1.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(4.0F, 7.0F, -1.0F));
+		legFrontFrontLeftUpper.addOrReplaceChild("legFrontFrontLeftLower", CubeListBuilder.create().texOffs(0, 17).addBox(0.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(15.0F, 1.0F, 0.0F));
+		
+		PartDefinition legFrontFrontRightUpper = partdefinition.addOrReplaceChild("legFrontFrontRightUpper", CubeListBuilder.create().texOffs(0, 17).addBox(-15.0F, -1.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(-4.0F, 7.0F, -1.0F));
+		legFrontFrontRightUpper.addOrReplaceChild("legFrontFrontRightLower", CubeListBuilder.create().texOffs(0, 17).addBox(-16.0F, 0.0F, -1.0F, 16.0F, 2.0F, 2.0F), PartPose.offset(-15.0F, 1.0F, 0.0F));
+		
+		PartDefinition armLeft = body.addOrReplaceChild("armLeft", CubeListBuilder.create().texOffs(0, 21).addBox(-2.0F, -1.0F, -1.0F, 10.0F, 2.0F, 2.0F), PartPose.offset(4.0F, 1.0F, -3.0F));
+		PartDefinition clawLeft = armLeft.addOrReplaceChild("clawLeft", CubeListBuilder.create().texOffs(0, 21).addBox(0.0F, -1.0F, -2.0F, 10.0F, 2.0F, 2.0F), PartPose.offset(8.0F, 0.0F, 1.0F));
+		clawLeft.addOrReplaceChild("pincherLeft", CubeListBuilder.create().texOffs(0, 25).addBox(0.0F, -1.0F, -2.0F, 5.0F, 2.0F, 2.0F), PartPose.offset(5.0F, 0.0F, 0.0F));
+		
+		PartDefinition armRight = body.addOrReplaceChild("armRight", CubeListBuilder.create().texOffs(0, 21).addBox(-8.0F, -1.0F, -1.0F, 10.0F, 2.0F, 2.0F), PartPose.offset(-4.0F, 1.0F, -3.0F));
+		PartDefinition clawRight = armRight.addOrReplaceChild("clawRight", CubeListBuilder.create().texOffs(0, 21).addBox(0.0F, -1.0F, 0.0F, 10.0F, 2.0F, 2.0F), PartPose.offset(-8.0F, 0.0F, 1.0F));
+		clawRight.addOrReplaceChild("pincherRight", CubeListBuilder.create().texOffs(0, 25).addBox(0.0F, -1.0F, 0.0F, 5.0F, 2.0F, 2.0F), PartPose.offset(5.0F, 0.0F, 0.0F));
+		
+		return LayerDefinition.create(meshdefinition, 64, 32);
 	}
 	
-	@Override
-	public Iterable<ModelPart> parts() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setupAnim(T p_102618_, float p_102619_, float p_102620_, float p_102621_, float p_102622_,
-			float p_102623_) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/*
 	public Iterable<ModelPart> parts() {
 		return ImmutableList.of(this.body, this.legBackBackRightUpper, this.legBackBackLeftUpper, this.legBackRightUpper, this.legBackLeftUpper, this.legFrontRightUpper, this.legFrontLeftUpper, this.legFrontFrontRightUpper, this.legFrontFrontLeftUpper);
 	}
@@ -224,14 +148,14 @@ public class KingCrabModel<T extends Entity> extends ListModel<T> {
 		this.legFrontFrontLeftLower.zRot = (float) Math.PI / 2;
 		this.legFrontFrontRightLower.zRot = -(float) Math.PI / 2;
 
-		float f3 = -(Mth.cos(limbSwing * 0.6662F * 2.0F + 0.0F) * 0.4F) * limbSwingAmount;
-		float f4 = -(Mth.cos(limbSwing * 0.6662F * 2.0F + (float) Math.PI) * 0.4F) * limbSwingAmount;
-		float f5 = -(Mth.cos(limbSwing * 0.6662F * 2.0F + ((float) Math.PI / 2F)) * 0.4F) * limbSwingAmount;
-		float f6 = -(Mth.cos(limbSwing * 0.6662F * 2.0F + ((float) Math.PI * 1.5F)) * 0.4F) * limbSwingAmount;
-		float f7 = Math.abs(Mth.sin(limbSwing * 0.6662F + 0.0F) * 0.4F) * limbSwingAmount;
-		float f8 = Math.abs(Mth.sin(limbSwing * 0.6662F + (float) Math.PI) * 0.4F) * limbSwingAmount;
-		float f9 = Math.abs(Mth.sin(limbSwing * 0.6662F + ((float) Math.PI / 2F)) * 0.4F) * limbSwingAmount;
-		float f10 = Math.abs(Mth.sin(limbSwing * 0.6662F + ((float) Math.PI * 1.5F)) * 0.4F) * limbSwingAmount;
+		float f3 = (float)  (-(Math.cos(limbSwing * 0.6662F * 2.0F + 0.0F) * 0.4F) * limbSwingAmount);
+		float f4 = (float)  (-(Math.cos(limbSwing * 0.6662F * 2.0F + (float) Math.PI) * 0.4F) * limbSwingAmount);
+		float f5 = (float)  (-(Math.cos(limbSwing * 0.6662F * 2.0F + ((float) Math.PI / 2F)) * 0.4F) * limbSwingAmount);
+		float f6 = (float)  (-(Math.cos(limbSwing * 0.6662F * 2.0F + ((float) Math.PI * 1.5F)) * 0.4F) * limbSwingAmount);
+		float f7 = (float)  (Math.abs(Math.sin(limbSwing * 0.6662F + 0.0F) * 0.4F) * limbSwingAmount);
+		float f8 = (float)  (Math.abs(Math.sin(limbSwing * 0.6662F + (float) Math.PI) * 0.4F) * limbSwingAmount);
+		float f9 = (float)  (Math.abs(Math.sin(limbSwing * 0.6662F + ((float) Math.PI / 2F)) * 0.4F) * limbSwingAmount);
+		float f10 = (float)  (Math.abs(Math.sin(limbSwing * 0.6662F + ((float) Math.PI * 1.5F)) * 0.4F) * limbSwingAmount);
 		
 		this.legBackBackRightUpper.yRot += f3;
 		this.legBackBackLeftUpper.yRot += -f3;
@@ -263,5 +187,5 @@ public class KingCrabModel<T extends Entity> extends ListModel<T> {
 		modelRenderer.yRot = y;
 		modelRenderer.zRot = z;
 	}
-	*/
+	
 }
--- src/main/java/com/technologica/client/renderer/entity/KingCrabRenderer.java
+++ src/main/java/com/technologica/client/renderer/entity/KingCrabRenderer.java
@@ -2,6 +2,7 @@ package com.technologica.client.renderer.entity;
 
 import com.technologica.Technologica;
 import com.technologica.client.model.KingCrabModel;
+import com.technologica.client.model.geom.TechnologicaModelLayers;
 import com.technologica.world.entity.animal.KingCrab;
 
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -12,7 +13,7 @@ public final class KingCrabRenderer extends MobRenderer<KingCrab, KingCrabModel<
 	private static final ResourceLocation KING_CRAB_TEXTURE = new ResourceLocation(Technologica.MODID, "textures/entity/king_crab.png");
 
 	public KingCrabRenderer(EntityRendererProvider.Context renderManagerIn) {
-		super(renderManagerIn, new KingCrabModel<>(), 1.1F);
+		super(renderManagerIn, new KingCrabModel<>(renderManagerIn.bakeLayer(TechnologicaModelLayers.KING_CRAB)), 1.1F);
 	}
 
 	@Override
