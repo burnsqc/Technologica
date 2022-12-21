eb5c02d4fdc82b2995402f96a540b58817e7028e
burnsqc <jonathan.r.mobley@gmail.com>
2022-12-21 09:57:23 -0700

    Ostrich

--- src/main/java/com/technologica/client/model/OstrichModel.java
+++ src/main/java/com/technologica/client/model/OstrichModel.java
@@ -1,5 +1,8 @@
 package com.technologica.client.model;
 
+import com.google.common.collect.ImmutableList;
+import com.technologica.world.entity.animal.Ostrich;
+
 import net.minecraft.client.model.AgeableListModel;
 import net.minecraft.client.model.QuadrupedModel;
 import net.minecraft.client.model.geom.ModelPart;
@@ -9,10 +12,10 @@ import net.minecraft.client.model.geom.builders.CubeListBuilder;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.model.geom.builders.MeshDefinition;
 import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
 
 public class OstrichModel<T extends AbstractHorse> extends AgeableListModel<T> {
-	/*
 	protected final ModelPart body;
 	private final ModelPart tail;
 	protected final ModelPart wingLeft;
@@ -20,160 +23,75 @@ public class OstrichModel<T extends AbstractHorse> extends AgeableListModel<T> {
 	protected final ModelPart neckLower;
 	protected final ModelPart neck;
 	protected final ModelPart head;
-	protected final ModelPart beak;
 	private final ModelPart legUpperLeft;
 	private final ModelPart legUpperRight;
 	private final ModelPart legLowerLeft;
 	private final ModelPart legLowerRight;
-	private final ModelPart talonInnerLeft;
-	private final ModelPart talonInnerRight;
 	private final ModelPart talonOuterLeft;
 	private final ModelPart talonOuterRight;
 	private final ModelPart saddle;
-	*/
 	
-	public OstrichModel() {
+	public OstrichModel(ModelPart modelPartIn) {
 		super(true, 24.0F, 1.36F, 2.7272F, 2.0F, 24.0F);
-		/*
-		this.texWidth = 64;
-		this.texHeight = 64;
-
-		this.body = new ModelPart(this, 0, 0);
-		this.body.addBox(-5.0F, -5.0F, -8.0F, 10.0F, 10.0F, 16.0F);
-		this.body.setPos(0.0F, 3.0F, 0.0F);
-		
-		this.tail = new ModelPart(this, 36, 0);
-		this.tail.addBox(-2.0F, -2.0F, 0.0F, 4.0F, 4.0F, 3.0F);
-		this.tail.setPos(0.0F, -2.0F, 8.0F);
-		this.body.addChild(this.tail);
-		
-		this.wingLeft = new ModelPart(this, 0, 26);
-		this.wingLeft.mirror = true;
-		this.wingLeft.addBox(0.0F, -5.0F, 0.0F, 1.0F, 10.0F, 12.0F);
-		this.wingLeft.setPos(5.0F, 0F, -7.0F);
-		this.body.addChild(this.wingLeft);
-		
-		this.wingRight = new ModelPart(this, 0, 26);
-		this.wingRight.addBox(-1.0F, -5.0F, 0.0F, 1.0F, 10.0F, 12.0F);
-		this.wingRight.setPos(-5.0F, 0F, -7.0F);
-		this.body.addChild(this.wingRight);
-
-		this.legUpperLeft = new ModelPart(this, 0, 26);
-		this.legUpperLeft.mirror = true;
-		this.legUpperLeft.addBox(-1.0F, 0.0F, 0.0F, 2.0F, 6.0F, 2.0F);
-		this.legUpperLeft.setPos(3.0F, 5.0F, 0.0F);
-		this.body.addChild(this.legUpperLeft);
-		
-		this.legUpperRight = new ModelPart(this, 0, 26);
-		this.legUpperRight.addBox(-1.0F, 0.0F, 0.0F, 2.0F, 6.0F, 2.0F);
-		this.legUpperRight.setPos(-3.0F, 5.0F, 0.0F);
-		this.body.addChild(this.legUpperRight);
-		
-		this.legLowerLeft = new ModelPart(this, 18, 26);
-		this.legLowerLeft.mirror = true;
-		this.legLowerLeft.addBox(-1.0F, 0.0F, -2.0F, 2.0F, 10.0F, 2.0F);
-		this.legLowerLeft.setPos(0.0F, 6.0F, 2.0F);
-		this.legUpperLeft.addChild(this.legLowerLeft);
-		
-		this.legLowerRight = new ModelPart(this, 18, 26);
-		this.legLowerRight.addBox(-1.0F, 0.0F, -2.0F, 2.0F, 10.0F, 2.0F);
-		this.legLowerRight.setPos(0.0F, 6.0F, 2.0F);
-		this.legUpperRight.addChild(this.legLowerRight);
-		
-		this.talonInnerLeft = new ModelPart(this, 0, 34);
-		this.talonInnerLeft.mirror = true;
-		this.talonInnerLeft.addBox(-1.0F, 9.0F, -5.0F, 1.0F, 1.0F, 3.0F);
-		this.legLowerLeft.addChild(talonInnerLeft);
-		
-		this.talonInnerRight = new ModelPart(this, 0, 34);
-		this.talonInnerRight.addBox(0.0F, 9.0F, -5.0F, 1.0F, 1.0F, 3.0F);
-		this.legLowerRight.addChild(talonInnerRight);
-		
-		this.talonOuterLeft = new ModelPart(this, 6, 34);
-		this.talonOuterLeft.mirror = true;
-		this.talonOuterLeft.addBox(0.0F, 9.0F, -2.0F, 1.0F, 1.0F, 2.0F);
-		this.talonOuterLeft.setPos(0.0F, 0.0F, -2.0F);
-		this.legLowerLeft.addChild(talonOuterLeft);
-		
-		this.talonOuterRight = new ModelPart(this, 6, 34);
-		this.talonOuterRight.addBox(-1.0F, 9.0F, -2.0F, 1.0F, 1.0F, 2.0F);
-		this.talonOuterRight.setPos(0.0F, 0.0F, -2.0F);
-		this.legLowerRight.addChild(talonOuterRight);
-		
-		this.neckLower = new ModelPart(this, 36, 7);
-		this.neckLower.addBox(-1.0F, -2.0F, -5.0F, 2.0F, 2.0F, 5.0F);
-		this.neckLower.setPos(0.0F, 2.0F, -8.0F);
-		
-		this.neck = new ModelPart(this, 50, 0);
-		this.neck.addBox(-1.0F, -10.0F, 0.0F, 2.0F, 10.0F, 2.0F);
-		this.neck.setPos(0.0F, 0.0F, -5.0F);
-		this.neckLower.addChild(this.neck);
-		
-		this.head = new ModelPart(this, 0, 0);
-		this.head.addBox(-2.0F, -13.0F, -2.0F, 4.0F, 3.0F, 4.0F);
-		this.neck.addChild(this.head);
-		
-		this.beak = new ModelPart(this, 0, 7);
-		this.beak.addBox(-1.0F, -11.0F, -4.0F, 2.0F, 1.0F, 2.0F);
-		this.head.addChild(this.beak);
-		
-		this.saddle = new ModelPart(this, 26, 26);
-		this.saddle.addBox(-5.0F, -5.0F, -5.0F, 10.0F, 10.0F, 9.0F, 0.5F);
-		this.body.addChild(this.saddle);	
-		*/
+		this.body = modelPartIn.getChild("body");
+		this.neckLower = modelPartIn.getChild("neckLower");
+		this.neck = neckLower.getChild("neck");
+		this.head = neck.getChild("head");
+		this.legUpperLeft = body.getChild("legUpperLeft");
+		this.legUpperRight = body.getChild("legUpperRight");
+		this.legLowerLeft = legUpperLeft.getChild("legLowerLeft");
+		this.legLowerRight = legUpperRight.getChild("legLowerRight");
+		this.talonOuterLeft = legLowerLeft.getChild("talonOuterLeft");
+		this.talonOuterRight = legLowerRight.getChild("talonOuterRight");
+		this.wingLeft = body.getChild("wingLeft");
+		this.wingRight = body.getChild("wingRight");
+		this.tail = body.getChild("tail");
+		this.saddle = body.getChild("saddle");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = QuadrupedModel.createBodyMesh(6, CubeDeformation.NONE);
-		PartDefinition partdefinition = meshdefinition.getRoot();
-		
-		partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 14).addBox(-2.0F, -4.0F, -4.0F, 4.0F, 4.0F, 4.0F), PartPose.offset(0.0F, 21.0F, -4.0F));
-		partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, 10.0F, -6.0F, 6.0F, 6.0F, 8.0F), PartPose.offset(0.0F, 6.0F, 2.0F));
-		
-		partdefinition.addOrReplaceChild("rightHindLeg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F), PartPose.offset(-2.0F, 21.0F, 3.0F));
-		partdefinition.addOrReplaceChild("leftHindLeg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F), PartPose.offset(2.0F, 21.0F, 3.0F));
-		partdefinition.addOrReplaceChild("rightFrontLeg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F), PartPose.offset(-2.0F, 21.0F, -3.0F));
-		partdefinition.addOrReplaceChild("leftFrontLeg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F), PartPose.offset(2.0F, 21.0F, -3.0F));
-		partdefinition.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(0, 22).addBox(-2.0F, 0.0F, 0.0F, 4.0F, 1.0F, 6.0F), PartPose.offset(0.0F, 14.0F, 2.0F));
-		
-		return LayerDefinition.create(meshdefinition, 32, 32);
+		PartDefinition root = meshdefinition.getRoot();
+		PartDefinition body = root.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-5.0F, -5.0F, -8.0F, 10.0F, 10.0F, 16.0F), PartPose.offset(0.0F, 3.0F, 0.0F));
+		body.addOrReplaceChild("wingLeft", CubeListBuilder.create().texOffs(0, 26).mirror().addBox(0.0F, -5.0F, 0.0F, 1.0F, 10.0F, 12.0F), PartPose.offset(5.0F, 0.0F, -7.0F));
+		body.addOrReplaceChild("wingRight", CubeListBuilder.create().texOffs(0, 26).addBox(-1.0F, -5.0F, 0.0F, 1.0F, 10.0F, 12.0F), PartPose.offset(-5.0F, 0.0F, -7.0F));
+		PartDefinition legUpperLeft = body.addOrReplaceChild("legUpperLeft", CubeListBuilder.create().mirror().texOffs(0, 26).addBox(-1.0F, 0.0F, 0.0F, 2.0F, 6.0F, 2.0F), PartPose.offset(3.0F, 5.0F, 0.0F));
+		PartDefinition legUpperRight = body.addOrReplaceChild("legUpperRight", CubeListBuilder.create().texOffs(0, 26).addBox(-1.0F, 0.0F, 0.0F, 2.0F, 6.0F, 2.0F), PartPose.offset(-3.0F, 5.0F, 0.0F));
+		PartDefinition legLowerLeft = legUpperLeft.addOrReplaceChild("legLowerLeft", CubeListBuilder.create().texOffs(18, 26).mirror().addBox(-1.0F, 0.0F, -2.0F, 2.0F, 10.0F, 2.0F), PartPose.offset(0.0F, 6.0F, 2.0F));
+		PartDefinition legLowerRight = legUpperRight.addOrReplaceChild("legLowerRight", CubeListBuilder.create().texOffs(18, 26).addBox(-1.0F, 0.0F, -2.0F, 2.0F, 10.0F, 2.0F), PartPose.offset(0.0F, 6.0F, 2.0F));
+		legLowerLeft.addOrReplaceChild("talonInnerLeft", CubeListBuilder.create().texOffs(0, 34).mirror().addBox(-1.0F, 9.0F, -5.0F, 1.0F, 1.0F, 3.0F), PartPose.offset(0.0F, 0.0F, 0.0F));
+		legLowerRight.addOrReplaceChild("talonInnerRight", CubeListBuilder.create().texOffs(0, 34).addBox(0.0F, 9.0F, -5.0F, 1.0F, 1.0F, 3.0F), PartPose.offset(0.0F, 0.0F, 0.0F));
+		legLowerLeft.addOrReplaceChild("talonOuterLeft", CubeListBuilder.create().texOffs(6, 34).mirror().addBox(0.0F, 9.0F, -2.0F, 1.0F, 1.0F, 2.0F), PartPose.offset(0.0F, 0.0F, -2.0F));
+		legLowerRight.addOrReplaceChild("talonOuterRight", CubeListBuilder.create().texOffs(6, 34).addBox(-1.0F, 9.0F, -2.0F, 1.0F, 1.0F, 2.0F), PartPose.offset(0.0F, 0.0F, -2.0F));
+		PartDefinition neckLower = root.addOrReplaceChild("neckLower", CubeListBuilder.create().texOffs(36, 7).addBox(-1.0F, -2.0F, -5.0F, 2.0F, 2.0F, 5.0F), PartPose.offset(0.0F, 2.0F, -8.0F));
+		PartDefinition neck = neckLower.addOrReplaceChild("neck", CubeListBuilder.create().texOffs(50, 0).addBox(-1.0F, -10.0F, 0.0F, 2.0F, 10.0F, 2.0F), PartPose.offset(0.0F, 0.0F, -5.0F));
+		PartDefinition head = neck.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-2.0F, -13.0F, -2.0F, 4.0F, 3.0F, 4.0F), PartPose.offset(0.0F, 0.0F, 0.0F));
+		head.addOrReplaceChild("beak", CubeListBuilder.create().texOffs(0, 7).addBox(-1.0F, -11.0F, -4.0F, 2.0F, 1.0F, 2.0F), PartPose.offset(0.0F, 0.0F, 0.0F));
+		body.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(36, 0).addBox(-2.0F, -2.0F, 0.0F, 4.0F, 4.0F, 3.0F), PartPose.offset(0.0F, -2.0F, 8.0F));
+		body.addOrReplaceChild("saddle", CubeListBuilder.create().texOffs(26, 26).mirror().addBox(-5.0F, -5.0F, -5.0F, 10.0F, 10.0F, 9.0F, new CubeDeformation(0.5F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
 	
 	@Override
 	protected Iterable<ModelPart> headParts() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected Iterable<ModelPart> bodyParts() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setupAnim(T p_102618_, float p_102619_, float p_102620_, float p_102621_, float p_102622_,
-			float p_102623_) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/*
-	public Iterable<ModelPart> headParts() {
 		return ImmutableList.of(this.neckLower);
 	}
 
+	@Override
 	protected Iterable<ModelPart> bodyParts() {
 		return ImmutableList.of(this.body);
 	}
 
+	
+	@Override
 	public void setupAnim(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netneckYaw, float neckPitch) {
 		this.talonOuterLeft.yRot = -((float) Math.PI / 6F);
 		this.talonOuterRight.yRot = ((float) Math.PI / 6F);
 		boolean flag = entityIn.isSaddled();
 		saddle.visible = flag;
 	}
-
+	
+	@Override
 	public void prepareMobModel(T entityIn, float limbSwing, float limbSwingAmount, float partialTick) {
 		float f = Mth.lerp(partialTick, entityIn.yBodyRotO, entityIn.yBodyRot);
 		float f1 = Mth.lerp(partialTick, entityIn.yHeadRotO, entityIn.yHeadRot);
@@ -190,11 +108,11 @@ public class OstrichModel<T extends AbstractHorse> extends AgeableListModel<T> {
 
 	    float f6 = entityIn.getStandAnim(partialTick);
 	    float f7 = 1.0F - f6;
-	    float f9 = (float)entityIn.tickCount + partialTick;
+	    float f9 = entityIn.tickCount + partialTick;
 		float f10 = entityIn.isInWater() ? 0.2F : 1.0F;
 		
 		boolean tailWagging = entityIn.tailCounter != 0;
-		boolean wingsFlapping = ((OstrichEntity) entityIn).wingCounter != 0;
+		boolean wingsFlapping = ((Ostrich) entityIn).wingCounter != 0;
 		
 		this.neckLower.y = 2.0F;
 		this.neckLower.z = -8.0F;
@@ -230,5 +148,4 @@ public class OstrichModel<T extends AbstractHorse> extends AgeableListModel<T> {
 			this.wingRight.yRot = 0.0F;
 		}
 	}
-	*/
 }
--- src/main/java/com/technologica/client/renderer/entity/OstrichRenderer.java
+++ src/main/java/com/technologica/client/renderer/entity/OstrichRenderer.java
@@ -2,6 +2,7 @@ package com.technologica.client.renderer.entity;
 
 import com.technologica.Technologica;
 import com.technologica.client.model.OstrichModel;
+import com.technologica.client.model.geom.TechnologicaModelLayers;
 import com.technologica.world.entity.animal.Ostrich;
 
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -9,13 +10,13 @@ import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
 public final class OstrichRenderer extends MobRenderer<Ostrich, OstrichModel<Ostrich>> {
-	private static final ResourceLocation OSTRICH_TEXTURE = new ResourceLocation(Technologica.MODID,
-			"textures/entity/ostrich.png");
+	private static final ResourceLocation OSTRICH_TEXTURE = new ResourceLocation(Technologica.MODID, "textures/entity/ostrich.png");
 
 	public OstrichRenderer(EntityRendererProvider.Context renderManagerIn) {
-		super(renderManagerIn, new OstrichModel<>(), 1.0F);
+		super(renderManagerIn, new OstrichModel<>(renderManagerIn.bakeLayer(TechnologicaModelLayers.OSTRICH)), 1.0F);
 	}
 
+	@Override
 	public ResourceLocation getTextureLocation(Ostrich entity) {
 		return OSTRICH_TEXTURE;
 	}
