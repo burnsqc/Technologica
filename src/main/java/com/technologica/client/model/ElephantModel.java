15c742a3b517452b0f218482819f7e2c106f66ca
burnsqc <jonathan.r.mobley@gmail.com>
2022-12-21 12:44:35 -0700

    Update ElephantModel.java

--- src/main/java/com/technologica/client/model/ElephantModel.java
+++ src/main/java/com/technologica/client/model/ElephantModel.java
@@ -12,9 +12,6 @@ import net.minecraft.client.model.geom.builders.MeshDefinition;
 import net.minecraft.client.model.geom.builders.PartDefinition;
 
 public class ElephantModel<T extends Elephant> extends QuadrupedModel<T> {
-
-	/*
-	private final ModelPart nose;
 	protected ModelPart earLeft;
 	protected ModelPart earRight;
 	protected ModelPart tuskLeft;
@@ -23,102 +20,40 @@ public class ElephantModel<T extends Elephant> extends QuadrupedModel<T> {
 	protected ModelPart trunk2;
 	protected ModelPart trunk3;
 	protected ModelPart tail;
-	*/
 	
 	public ElephantModel(ModelPart modelPartIn) {
 		super(modelPartIn, true, 10.0F, 4.0F, 2.0F, 2.0F, 24);
-		
-		/*
-		this.texWidth = 256;
-		this.texHeight = 128;
-
-		this.head = new ModelPart(this, 135, 0);
-		this.head.addBox(-12.0F, -12.0F, -24.0F, 24.0F, 24.0F, 24.0F);
-		this.head.setPos(0.0F, -28.0F, -32.0F);
-		
-		this.nose = new ModelPart(this, 0, 104);
-		this.nose.addBox(-6.0F, -6.0F, -30.0F, 12.0F, 18.0F, 6.0F);
-		this.head.addChild(this.nose);
-		
-		this.earLeft = new ModelPart(this, 0, 0);
-		this.earLeft.addBox(-1.0F, -10.0F, 0.0F, 1.0F, 24.0F, 24.0F);
-		this.earLeft.setPos(12.0F, -2.0F, -10.0F);
-		this.head.addChild(this.earLeft);
-		
-		this.earRight = new ModelPart(this, 0, 0);
-		this.earRight.addBox(0.0F, -10.0F, 0.0F, 1.0F, 24.0F, 24.0F);
-		this.earRight.setPos(-12.0F, -2.0F, -10.0F);
-		this.head.addChild(this.earRight);
-		
-		this.tuskLeft = new ModelPart(this, 0, 0);
-		this.tuskLeft.addBox(-2.0F, 0.0F, -4.0F, 4.0F, 16.0F, 4.0F);
-		this.tuskLeft.setPos(9.0F, 12.0F, -19.0F);
-		this.head.addChild(this.tuskLeft);
-		
-		this.tuskRight = new ModelPart(this, 0, 0);
-		this.tuskRight.addBox(-2.0F, 0.0F, -4.0F, 4.0F, 16.0F, 4.0F);
-		this.tuskRight.setPos(-9.0F, 12.0F, -19.0F);
-		this.head.addChild(this.tuskRight);
-		
-		this.trunk1 = new ModelPart(this, 200, 48);
-		this.trunk1.addBox(-4.0F, -8.0F, 0.0F, 8.0F, 20.0F, 8.0F);
-		this.trunk1.setPos(0.0F, 12.0F, -30.0F);
-		this.head.addChild(this.trunk1);
-		
-		this.trunk2 = new ModelPart(this, 208, 0);
-		this.trunk2.addBox(-3.5F, 0.0F, 0.0F, 7.0F, 12.0F, 7.0F);
-		this.trunk2.setPos(0.0F, 12.0F, 0.0F);
-		this.trunk1.addChild(this.trunk2);
-		
-		this.trunk3 = new ModelPart(this, 136, 0);
-		this.trunk3.addBox(-3.0F, 0.0F, 0.0F, 6.0F, 12.0F, 6.0F);
-		this.trunk3.setPos(0.0F, 12.0F, 0.0F);
-		this.trunk2.addChild(this.trunk3);
-		
-		this.body = new ModelPart(this, 0, 0);
-		this.body.addBox(-18.0F, -20.0F, -32.0F, 36.0F, 40.0F, 64.0F);
-		this.body.setPos(0.0F, -20.0F, 0.0F);
-		
-		this.tail = new ModelPart(this, 236, 0);
-		this.tail.addBox(-1.5F, 0.0F, -3.0F, 3.0F, 32.0F, 3.0F);
-		this.tail.setPos(0.0F, -8.0F, 32.0F);
-		this.body.addChild(this.tail);
-
-		this.leg0 = new ModelPart(this, 188, 92);
-		this.leg0.addBox(-6.0F, 0.0F, -6.0F, 12.0F, 24.0F, 12.0F);
-		this.leg0.setPos(-12.0F, 0.0F, 26.0F);
-
-		this.leg1 = new ModelPart(this, 188, 92);
-	    this.leg1.addBox(-6.0F, 0.0F, -6.0F, 12.0F, 24.0F, 12.0F);
-	    this.leg1.setPos(12.0F, 0.0F, 26.0F);
-	      
-	    this.leg2 = new ModelPart(this, 188, 92);
-	    this.leg2.addBox(-6.0F, 0.0F, -6.0F, 12.0F, 24.0F, 12.0F);
-	    this.leg2.setPos(-12.0F, 0.0F, -26.0F);
-		
-	    this.leg3 = new ModelPart(this, 188, 92);
-	    this.leg3.addBox(-6.0F, 0.0F, -6.0F, 12.0F, 24.0F, 12.0F);
-	    this.leg3.setPos(12.0F, 0.0F, -26.0F);
-	    */
+		this.tuskLeft = this.head.getChild("tusk_left");
+		this.tuskRight = this.head.getChild("tusk_right");
+		this.earLeft = this.head.getChild("ear_left");
+		this.earRight = this.head.getChild("ear_right");
+		this.trunk1 = this.head.getChild("trunk1");
+		this.trunk2 = this.trunk1.getChild("trunk2");
+		this.trunk3 = this.trunk2.getChild("trunk3");
+		this.tail = this.body.getChild("tail");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = QuadrupedModel.createBodyMesh(6, CubeDeformation.NONE);
-		PartDefinition partdefinition = meshdefinition.getRoot();
-		
-		partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 14).addBox(-2.0F, -4.0F, -4.0F, 4.0F, 4.0F, 4.0F), PartPose.offset(0.0F, 21.0F, -4.0F));
-		partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, 10.0F, -6.0F, 6.0F, 6.0F, 8.0F), PartPose.offset(0.0F, 6.0F, 2.0F));
-		
-		partdefinition.addOrReplaceChild("rightHindLeg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F), PartPose.offset(-2.0F, 21.0F, 3.0F));
-		partdefinition.addOrReplaceChild("leftHindLeg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F), PartPose.offset(2.0F, 21.0F, 3.0F));
-		partdefinition.addOrReplaceChild("rightFrontLeg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F), PartPose.offset(-2.0F, 21.0F, -3.0F));
-		partdefinition.addOrReplaceChild("leftFrontLeg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 1.0F, -1.0F, 2.0F, 2.0F, 2.0F), PartPose.offset(2.0F, 21.0F, -3.0F));
-		partdefinition.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(0, 22).addBox(-2.0F, 0.0F, 0.0F, 4.0F, 1.0F, 6.0F), PartPose.offset(0.0F, 14.0F, 2.0F));
-		
-		return LayerDefinition.create(meshdefinition, 32, 32);
+		PartDefinition root = meshdefinition.getRoot();
+		PartDefinition body = root.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-18.0F, -20.0F, -32.0F, 36.0F, 40.0F, 64.0F), PartPose.offset(0.0F, -20.0F, 0.0F));
+		PartDefinition head = root.addOrReplaceChild("head", CubeListBuilder.create().texOffs(135, 0).addBox(-12.0F, -12.0F, -24.0F, 24.0F, 24.0F, 24.0F), PartPose.offset(0.0F, -28.0F, -32.0F));
+		head.addOrReplaceChild("nose", CubeListBuilder.create().texOffs(0, 104).addBox(-6.0F, -6.0F, -30.0F, 12.0F, 18.0F, 6.0F), PartPose.offset(0.0F, 0.0F, 0.0F));
+		head.addOrReplaceChild("ear_left", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, -10.0F, 0.0F, 1.0F, 24.0F, 24.0F), PartPose.offset(12.0F, -2.0F, -10.0F));
+		head.addOrReplaceChild("ear_right", CubeListBuilder.create().texOffs(0, 0).addBox(0.0F, -10.0F, 0.0F, 1.0F, 24.0F, 24.0F), PartPose.offset(-12.0F, -2.0F, -10.0F));
+		head.addOrReplaceChild("tusk_left", CubeListBuilder.create().texOffs(0, 0).addBox(-2.0F, 0.0F, -4.0F, 4.0F, 16.0F, 4.0F), PartPose.offset(9.0F, 12.0F, -19.0F));
+		head.addOrReplaceChild("tusk_right", CubeListBuilder.create().texOffs(0, 0).addBox(-2.0F, 0.0F, -4.0F, 4.0F, 16.0F, 4.0F), PartPose.offset(-9.0F, 12.0F, -19.0F));
+		PartDefinition trunk1 = head.addOrReplaceChild("trunk1", CubeListBuilder.create().texOffs(200, 48).addBox(-4.0F, -8.0F, 0.0F, 8.0F, 20.0F, 8.0F), PartPose.offset(0.0F, 12.0F, -30.0F));
+		PartDefinition trunk2 = trunk1.addOrReplaceChild("trunk2", CubeListBuilder.create().texOffs(208, 0).addBox(-3.5F, 0.0F, 0.0F, 7.0F, 12.0F, 7.0F), PartPose.offset(0.0F, 12.0F, 0.0F));
+		trunk2.addOrReplaceChild("trunk3", CubeListBuilder.create().texOffs(136, 0).addBox(-3.0F, 0.0F, 0.0F, 6.0F, 12.0F, 6.0F), PartPose.offset(0.0F, 12.0F, 0.0F));
+		root.addOrReplaceChild("right_hind_leg", CubeListBuilder.create().texOffs(188, 92).addBox(-6.0F, 0.0F, -6.0F, 12.0F, 24.0F, 12.0F), PartPose.offset(-12.0F, 0.0F, 26.0F));
+		root.addOrReplaceChild("left_hind_leg", CubeListBuilder.create().texOffs(188, 92).addBox(-6.0F, 0.0F, -6.0F, 12.0F, 24.0F, 12.0F), PartPose.offset(12.0F, 0.0F, 26.0F));
+		root.addOrReplaceChild("right_front_leg", CubeListBuilder.create().texOffs(188, 92).addBox(-6.0F, 0.0F, -6.0F, 12.0F, 24.0F, 12.0F), PartPose.offset(-12.0F, 0.0F, -26.0F));
+		root.addOrReplaceChild("left_front_leg", CubeListBuilder.create().texOffs(188, 92).addBox(-6.0F, 0.0F, -6.0F, 12.0F, 24.0F, 12.0F), PartPose.offset(12.0F, 0.0F, -26.0F));
+		body.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(236, 0).addBox(-1.5F, 0.0F, -3.0F, 3.0F, 32.0F, 3.0F), PartPose.offset(0.0F, -8.0F, 32.0F));
+		return LayerDefinition.create(meshdefinition, 256, 128);
 	}
 	
-	/*
 	@Override
 	public void setupAnim(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 		this.tuskLeft.xRot = -(float) Math.PI / 10.0F;
@@ -130,5 +65,4 @@ public class ElephantModel<T extends Elephant> extends QuadrupedModel<T> {
 		this.earLeft.yRot = (float) Math.PI / 5.0F;
 		this.earRight.yRot = -(float) Math.PI / 5.0F;
 	}
-	*/
 }
