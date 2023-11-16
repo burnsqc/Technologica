b50be45a7653cae198c4683584ec0a58afcf2e80
burnsqc <jonathan.r.mobley@gmail.com>
2023-11-16 08:05:30 -0700

    Update MonitorRenderer.java

--- src/main/java/com/technologica/client/renderer/blockentity/MonitorRenderer.java
+++ src/main/java/com/technologica/client/renderer/blockentity/MonitorRenderer.java
@@ -33,7 +33,7 @@ public class MonitorRenderer implements BlockEntityRenderer<MonitorBlockEntity> 
 		matrixStackIn.mulPose(Axis.YP.rotationDegrees(f1));
 		matrixStackIn.translate(0.0D, 0.24D, 0.376D);
 		matrixStackIn.scale(0.003F, -0.003F, 0.003F);
-		int i1 = 0;
+		int i1 = 8439583;
 		for (int k1 = 0; k1 < 16; ++k1) {
 			Component text = Component.literal(tileEntityIn.getText(k1).getString()).setStyle(FULLSPACE_FONT_STYLE);
 			font.drawInBatch(text, -96.0F, k1 * 10.0F - 20.0F, i1, false, matrixStackIn.last().pose(), bufferIn, Font.DisplayMode.POLYGON_OFFSET, 0, combinedLightIn);
